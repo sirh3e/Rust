3a465931754d3402c7bf3203873d215b38f4f35c
sirh3e <sirh3e@protonmail.com>
2020-11-22 14:47:00 +0100

    added unit test Option_Construct_None_FromValue

--- test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
+++ test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
@@ -27,5 +27,29 @@ namespace Sirh3e.Rust.Option.Test
                 option.Unwrap().Should().Be(42);
             }
         }
+
+        [Fact]
+        public void Option_Construct_None_FromNull()
+        {
+            {
+                var option = Option<string>.Some(null);
+
+                option.IsSome.Should().BeFalse();
+                option.IsNone.Should().BeTrue();
+
+                Action action = () => option.Unwrap();
+                action.Should().ThrowExactly<NotImplementedException>();
+            }
+            
+            {
+                var option = Option<object>.Some(null);
+
+                option.IsSome.Should().BeFalse();
+                option.IsNone.Should().BeTrue();
+
+                Action action = () => option.Unwrap();
+                action.Should().ThrowExactly<NotImplementedException>();
+            }
+        }
     }
 }
