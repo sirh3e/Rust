4055c8dec6406d3a4fb423777349c4d0e4c26bcf
sirh3e <sirh3e@protonmail.com>
2020-11-22 14:52:33 +0100

    added unit test Option_Unwrap

--- test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
+++ test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
@@ -51,5 +51,28 @@ namespace Sirh3e.Rust.Option.Test
                 action.Should().ThrowExactly<NotImplementedException>();
             }
         }
+
+        [Fact]
+        public void Option_Unwrap()
+        {
+            {
+                var option = Option<string>.Some("air");
+
+                option.IsSome.Should().BeTrue();
+                option.IsNone.Should().BeFalse();
+
+                option.Unwrap().Should().Be("air");
+            }
+            
+            {
+                var option = Option<string>.Some(null);
+
+                option.IsSome.Should().BeFalse();
+                option.IsNone.Should().BeTrue();
+
+                Action action = () => option.Unwrap();
+                action.Should().ThrowExactly<NotImplementedException>();
+            }
+        }
     }
 }
