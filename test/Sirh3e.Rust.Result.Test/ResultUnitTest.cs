75ccca2d0f44afda67860d498dd8722ddad9ac40
sirh3e <sirh3e@protonmail.com>
2020-12-06 23:04:48 +0100

    refactored Result_MapOrElse into own file

--- test/Sirh3e.Rust.Result.Test/ResultUnitTest.cs
+++ test/Sirh3e.Rust.Result.Test/ResultUnitTest.cs
@@ -47,28 +47,7 @@ namespace Sirh3e.Rust.Result.Test
 
 
 
-        [Fact]
-        public void Result_MapOrElse()
-        {
-            var k = 21;
-            {
-                var result = Result<string, string>.Ok("foo");
 
-                result.IsOk.Should().BeTrue();
-                result.IsErr.Should().BeFalse();
-
-                result.MapOrElse(s => k * 2, s => s.Length).Should().Be(3);
-            }
-
-            {
-                var result = Result<string, string>.Err("bar");
-
-                result.IsOk.Should().BeFalse();
-                result.IsErr.Should().BeTrue();
-
-                result.MapOrElse(s => k * 2, s => s.Length).Should().Be(42);
-            }
-        }
 
         [Fact]
         public void Result_MapErr()
