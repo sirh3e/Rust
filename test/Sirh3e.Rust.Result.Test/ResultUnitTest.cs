64d5b87dd066879f31c2fa6310e1935479a41448
sirh3e <sirh3e@protonmail.com>
2020-12-06 23:07:55 +0100

    refactored Result_Match into own file

--- test/Sirh3e.Rust.Result.Test/ResultUnitTest.cs
+++ test/Sirh3e.Rust.Result.Test/ResultUnitTest.cs
@@ -51,35 +51,7 @@ namespace Sirh3e.Rust.Result.Test
 
 
 
-        [Fact]
-        public void Result_Match_Ok()
-        {
-            var result = Result<string, string>.Ok("foo");
 
-            result.IsOk.Should().BeTrue();
-            result.IsErr.Should().BeFalse();
-
-            result.Match(s =>
-            {
-                s.Length.Should().Be(3);
-                s.Should().Be("foo");
-            }, _ => throw new NotImplementedException());
-        }
-
-        [Fact]
-        public void Result_Match_Err()
-        {
-            var result = Result<string, string>.Err("bar");
-
-            result.IsOk.Should().BeFalse();
-            result.IsErr.Should().BeTrue();
-
-            result.Match(_ => throw new NotImplementedException(), s =>
-            {
-                s.Length.Should().Be(3);
-                s.Should().Be("bar");
-            });
-        }
 
 
     }
