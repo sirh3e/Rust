9a36783e1d8aa39515555276f804b6c45bb6702b
sirh3e <sirh3e@protonmail.com>
2020-12-06 23:06:10 +0100

    refactored Result_MapErr into own file

--- test/Sirh3e.Rust.Result.Test/ResultUnitTest.cs
+++ test/Sirh3e.Rust.Result.Test/ResultUnitTest.cs
@@ -49,43 +49,7 @@ namespace Sirh3e.Rust.Result.Test
 
 
 
-        [Fact]
-        public void Result_MapErr()
-        {
-            Func<uint, string> stringify = s => $"error code {s}";
 
-            {
-                var result = Result<uint, uint>.Ok(2);
-
-                result.IsOk.Should().BeTrue();
-                result.IsErr.Should().BeFalse();
-
-                var t = result.MapErr(stringify);
-                t.IsOk.Should().BeTrue();
-                t.IsErr.Should().BeFalse();
-
-                t.Ok().IsSome.Should().BeTrue();
-                t.Ok().IsNone.Should().BeFalse();
-
-                t.Ok().Unwrap().Should().Be(2);
-            }
-
-            {
-                var result = Result<uint, uint>.Err(13);
-
-                result.IsOk.Should().BeFalse();
-                result.IsErr.Should().BeTrue();
-
-                var t = result.MapErr(stringify);
-                t.IsOk.Should().BeFalse();
-                t.IsErr.Should().BeTrue();
-
-                t.Err().IsSome.Should().BeTrue();
-                t.Err().IsNone.Should().BeFalse();
-
-                t.Err().Unwrap().Should().Be("error code 13");
-            }
-        }
 
         [Fact]
         public void Result_Match_Ok()
