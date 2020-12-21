8bf4bf89979579dd942fc299421d6198c5a948e2
sirh3e <sirh3e@protonmail.com>
2020-12-21 04:27:54 +0100

    added Flatten

--- src/Sirh3e.Rust.Result/Methods/Result.Flatten.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.Flatten.cs
@@ -1,7 +1,15 @@
-﻿namespace Sirh3e.Rust.Result
+﻿using System;
+
+namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
+        public Result<TOk, TErr> Flatten()
+        {
+            if (this is not Result<Result<TOk, TErr>, TErr> result)
+                throw new NotImplementedException();
 
+            return result.IsOk ? result._ok : Err(result._err);
+        }
     }
 }
