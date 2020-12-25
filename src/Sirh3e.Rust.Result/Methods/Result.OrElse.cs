ccd2cafbe07a4e2ceb81fb75f2a89c69f37a3595
sirh3e <sirh3e@protonmail.com>
2020-12-25 22:10:01 +0100

    added Result.OrElse.cs

--- src/Sirh3e.Rust.Result/Methods/Result.OrElse.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.OrElse.cs
@@ -4,6 +4,13 @@ namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
-        public Result<TOk, TErr> OrElse(Func<TErr, TOk> other) => Ok(IsOk ? _ok : other(_err));
+        public Result<TOk, TErr> OrElse(Func<TErr, Result<TOk, TErr>> other)
+        {
+            if (IsOk)
+                return this;
+            if (other is null)
+                throw new ArgumentNullException(nameof(other));
+            return other(_err);
+        }
     }
 }
