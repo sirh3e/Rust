392e6f5cda626e60b51e30db935eeb9680f6d905
sirh3e <sirh3e@protonmail.com>
2020-12-25 21:07:18 +0100

    added Result.UnwrapOrElse.cs

--- src/Sirh3e.Rust.Result/Methods/Result.UnwrapOrElse.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.UnwrapOrElse.cs
@@ -1,7 +1,16 @@
-﻿namespace Sirh3e.Rust.Result
+﻿using System;
+
+namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
-
+        public TOk UnwrapOrElse(Func<TErr, TOk> mapper)
+        {
+            if (IsOk)
+                return _ok;
+            if (mapper is null)
+                throw new ArgumentNullException(nameof(mapper));
+            return mapper(_err);
+        }
     }
 }
