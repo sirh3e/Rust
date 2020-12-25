a7a50446224763d54c60a3dd477183beb4ae8b49
sirh3e <sirh3e@protonmail.com>
2020-12-25 21:54:58 +0100

    added Result.ContainsErr.UnitTest.cs

--- test/Sirh3e.Rust.Result.Test/Methods/Result.ContainsErr.UnitTest.cs
+++ test/Sirh3e.Rust.Result.Test/Methods/Result.ContainsErr.UnitTest.cs
@@ -1,7 +1,45 @@
-﻿namespace Sirh3e.Rust.Result.Test
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Sirh3e.Rust.Result.Test
 {
     public partial class ResultUnitTest
     {
+        [Fact]
+        public void Result_ContainsErr()
+        {
+            {
+                var x = Result<uint, string>.Ok(2);
 
+                x.IsOk.Should().BeTrue();
+                x.IsErr.Should().BeFalse();
+
+                x.ContainsErr("Some error message")
+                    .Should()
+                    .BeFalse();
+            }
+
+            {
+                var x = Result<uint, string>.Err("Some error message");
+
+                x.IsOk.Should().BeFalse();
+                x.IsErr.Should().BeTrue();
+
+                x.ContainsErr("Some error message")
+                    .Should()
+                    .BeTrue();
+            }
+
+            {
+                var x = Result<uint, string>.Err("Some other error message");
+
+                x.IsOk.Should().BeFalse();
+                x.IsErr.Should().BeTrue();
+
+                x.ContainsErr("Some other error message")
+                    .Should()
+                    .BeTrue();
+            }
+        }
     }
 }
