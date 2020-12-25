50db2e2968db93e18444a7945a125d1d8dcbdba2
sirh3e <sirh3e@protonmail.com>
2020-12-25 22:40:01 +0100

    fixed ContainsErr and added Result Transform

---
+++ src/Sirh3e.Rust.Result/Extensions/Result.Transpose.Extension.cs
@@ -0,0 +1,17 @@
+﻿using Sirh3e.Rust.Option;
+
+namespace Sirh3e.Rust.Result.Extensions
+{
+    public static partial class ResultExtension
+    {
+        public static Option<Result<TOk, TErr>> Transpose<TOk, TErr>(this Result<Option<TOk>, TErr> result)
+        {
+            if (!result.IsOk)
+                return Option<Result<TOk, TErr>>.Some(Result<TOk, TErr>.Err(result.Err().Unwrap()));
+
+            var option = result.Ok().Unwrap();
+
+            return option.IsNone ? Option<Result<TOk, TErr>>.None : Option<Result<TOk, TErr>>.Some(Result<TOk, TErr>.Ok(option.Unwrap()));
+        }
+    }
+}
---
+++ test/Sirh3e.Rust.Result.Test/Methods/Result.Transpose.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Sirh3e.Rust.Option;
+using Sirh3e.Rust.Result.Extensions;
+using Xunit;
+
+namespace Sirh3e.Rust.Result.Test
+{
+    public partial class ResultUnitTest
+    {
+        [Fact]
+        public void Result_Transpose()
+        {
+            var x = Result<Option<int>, string>.Ok(Option<int>.Some(5));
+            var y = Option<Result<int, string>>.Some(Result<int, string>.Ok(5));
+
+            x.Transpose().Should().Equals(y);
+        }
+    }
+}
