f711fc365eeeeab6b1647e5a4643a1bf62174ee6
sirh3e <sirh3e@protonmail.com>
2020-12-25 21:04:34 +0100

    added Result.UnwrapOrDefault.cs

--- src/Sirh3e.Rust.Result/Methods/Result.UnwrapOrDefault.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.UnwrapOrDefault.cs
@@ -1,7 +1,9 @@
-﻿namespace Sirh3e.Rust.Result
+﻿using System;
+
+namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
-
+        public TOk UnwrapOrDefault() => IsOk ? _ok : Activator.CreateInstance<TOk>();
     }
 }
