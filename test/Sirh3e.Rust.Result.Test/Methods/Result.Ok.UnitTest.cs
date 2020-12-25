a8367055b71ee49305aa6938d6c24b43f040af9d
sirh3e <sirh3e@protonmail.com>
2020-12-25 16:59:57 +0100

    added Result.Ok.UnitTest.cs

---
+++ test/Sirh3e.Rust.Result.Test/Methods/Result.Ok.UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Sirh3e.Rust.Option;
+using Xunit;
+
+namespace Sirh3e.Rust.Result.Test
+{
+    public partial class ResultUnitTest
+    {
+        [Fact]
+        public void Result_Ok()
+        {
+            {
+                var x = Result<uint, string>.Ok(2);
+
+                x.IsOk.Should().BeTrue();
+                x.IsErr.Should().BeFalse();
+
+                var option = x.Ok();
+
+                option.IsSome.Should().BeTrue();
+                option.IsNone.Should().BeFalse();
+
+                option.Unwrap().Should().Be(2);
+            }
+
+            {
+                var x = Result<uint, string>.Err("Nothing here");
+
+                x.IsOk.Should().BeFalse();
+                x.IsErr.Should().BeTrue();
+
+                var option = x.Ok();
+
+                option.IsSome.Should().BeFalse();
+                option.IsNone.Should().BeTrue();
+            }
+        }
+    }
+}
