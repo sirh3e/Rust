6a37006872e548df1f908136929346adb17293a2
sirh3e <sirh3e@protonmail.com>
2020-12-25 23:17:36 +0100

    added IEquatable on None

--- src/Sirh3e.Rust.Option/None.cs
+++ src/Sirh3e.Rust.Option/None.cs
@@ -1,7 +1,34 @@
-﻿namespace Sirh3e.Rust.Option
+﻿using System;
+
+namespace Sirh3e.Rust.Option
 {
-    public struct None
+    public struct None : IEquatable<None>
     {
         public static readonly None Value = default;
+
+        public bool Equals(None other)
+        {
+            return GetHashCode().Equals(other.GetHashCode());
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is None other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return -1;
+        }
+
+        public static bool operator ==(None left, None right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(None left, None right)
+        {
+            return !left.Equals(right);
+        }
     }
 }
