a80dc27e68428acf8c2b622901bab26da8441623
sirh3e <sirh3e@protonmail.com>
2020-12-26 21:02:23 +0100

    Added Summary for Result.Err.cs removed unused ,

--- src/Sirh3e.Rust.Result/Methods/Result.Err.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.Err.cs
@@ -4,11 +4,16 @@ namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
+        /// <summary>
+        /// Converts from Result&lt;TOk, TErr&gt; to Option&lt;TErr&gt;.
+        /// Converts self into an Option&lt;TErr&gt;, consuming self, and discarding the success value, if any.
+        /// </summary>
+        /// <returns></returns>
         public Option<TErr> Err()
             => IsErr switch
             {
                 true => Option<TErr>.Some(_err),
-                false => Option<TErr>.None,
+                false => Option<TErr>.None
             };
     }
 }
