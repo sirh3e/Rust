15a0d89e23f2d2a94c0204b5405051029dba7044
sirh3e <sirh3e@protonmail.com>
2020-12-26 21:55:03 +0100

    Added Summary for Result.UnwrapOr.cs added {} on if

--- src/Sirh3e.Rust.Result/Methods/Result.UnwrapOr.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.UnwrapOr.cs
@@ -4,12 +4,25 @@ namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
+        /// <summary>
+        /// Returns the contained Ok value or a default
+        /// Consumes the self argument then, if Ok, returns the contained value, otherwise if Err, returns the default value for that type.
+        /// </summary>
+        /// <param name="default"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public TOk UnwrapOr(TOk @default)
         {
             if (IsOk)
+            {
                 return _ok;
+            }
+
             if (@default is null)
+            {
                 throw new ArgumentNullException(nameof(@default));
+            }
+
             return @default;
         }
     }
