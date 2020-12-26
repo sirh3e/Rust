dd63fd858be22bbaad3c41be9b89f69ef4d92b07
sirh3e <sirh3e@protonmail.com>
2020-12-26 21:36:18 +0100

    Added Summary for Result.MapOr.cs added {} on ifs renamed U -> T renamed mapper into map

--- src/Sirh3e.Rust.Result/Methods/Result.MapOr.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.MapOr.cs
@@ -4,13 +4,32 @@ namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
-        public U MapOr<U>(U @default, Func<TOk, U> mapper)
+        /// <summary>
+        /// Applies a function to the contained value (if Ok), or returns the provided default (if Err).
+        /// Arguments passed to map_or are eagerly evaluated; if you are passing the result of a function call, it is recommended to use map_or_else, which is lazily evaluated.
+        /// </summary>
+        /// <param name="default"></param>
+        /// <param name="map"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Throws only if map is null or default is null</exception>
+        public T MapOr<T>(T @default, Func<TOk, T> map)
         {
-            if (!IsErr)
-                return mapper(_ok);
+            if (IsOk)
+            {
+                if (map is null)
+                {
+                    throw new ArgumentNullException(nameof(map));
+                }
+
+                return map(_ok);
+            }
 
             if (@default is null)
+            {
                 throw new ArgumentNullException(nameof(@default));
+            }
+
             return @default;
         }
     }
