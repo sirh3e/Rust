2fdd04214ea14fc620afd975a3a1f6b93f17767d
sirh3e <sirh3e@protonmail.com>
2020-12-26 21:56:55 +0100

    Added Summary for Result.UnwrapOrDefault.cs fixed Result.WnwrapOr.cs summary

--- src/Sirh3e.Rust.Result/Methods/Result.UnwrapOr.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.UnwrapOr.cs
@@ -5,8 +5,8 @@ namespace Sirh3e.Rust.Result
     public readonly partial struct Result<TOk, TErr>
     {
         /// <summary>
-        /// Returns the contained Ok value or a default
-        /// Consumes the self argument then, if Ok, returns the contained value, otherwise if Err, returns the default value for that type.
+        /// Returns the contained Ok value or a provided default.
+        /// Arguments passed to unwrap_or are eagerly evaluated; if you are passing the result of a function call, it is recommended to use unwrap_or_else, which is lazily evaluated.
         /// </summary>
         /// <param name="default"></param>
         /// <returns></returns>
--- src/Sirh3e.Rust.Result/Methods/Result.UnwrapOrDefault.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.UnwrapOrDefault.cs
@@ -4,6 +4,14 @@ namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
-        public TOk UnwrapOrDefault() => IsOk ? _ok : Activator.CreateInstance<TOk>();
+        /// <summary>
+        /// Returns the contained Ok value or a default
+        /// Consumes the self argument then, if Ok, returns the contained value, otherwise if Err, returns the default value for that type.
+        /// </summary>
+        /// <returns></returns>
+        public TOk UnwrapOrDefault()
+            => IsOk ?
+                _ok :
+                Activator.CreateInstance<TOk>();
     }
 }
