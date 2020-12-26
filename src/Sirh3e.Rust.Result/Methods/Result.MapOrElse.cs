41d27a9e14da1511d829c6f970a5b054030bc21e
sirh3e <sirh3e@protonmail.com>
2020-12-26 21:38:18 +0100

    Added Summary for Result.MapOrElse.cs added {} on ifs renamed U -> T renamed func into map

--- src/Sirh3e.Rust.Result/Methods/Result.MapOrElse.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.MapOrElse.cs
@@ -4,17 +4,31 @@ namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
-        public U MapOrElse<U>(Func<TErr, U> @default, Func<TOk, U> func)
+        /// <summary>
+        /// Maps a Result&lt;TOk, TErr&gt; to T by applying a function to a contained Ok value, or a fallback function to a contained Err value.
+        /// This function can be used to unpack a successful result while handling an error.
+        /// </summary>
+        /// <param name="default"></param>
+        /// <param name="map"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public T MapOrElse<T>(Func<TErr, T> @default, Func<TOk, T> map)
         {
             if (IsOk)
             {
-                if (func is null)
-                    throw new ArgumentNullException(nameof(func));
-                return func(_ok);
+                if (map is null)
+                {
+                    throw new ArgumentNullException(nameof(map));
+                }
+
+                return map(_ok);
             }
 
             if (@default is null)
+            {
                 throw new ArgumentNullException(nameof(@default));
+            }
 
             return @default(_err);
         }
