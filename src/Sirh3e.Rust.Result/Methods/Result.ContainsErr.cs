8ac12a26982b164cb7a0e67e9caf51ad7181e622
sirh3e <sirh3e@protonmail.com>
2020-12-26 20:57:43 +0100

    Added Summary for Result.ContainsErr.cs

--- src/Sirh3e.Rust.Result/Methods/Result.ContainsErr.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.ContainsErr.cs
@@ -2,6 +2,11 @@
 {
     public readonly partial struct Result<TOk, TErr>
     {
+        /// <summary>
+        /// Returns true if the result is an Err value containing the given value.
+        /// </summary>
+        /// <param name="err"></param>
+        /// <returns>Returns true if the result is an Err value containing the given value.</returns>
         public bool ContainsErr(TErr err) => IsErr && err.Equals(_err);
     }
 }
