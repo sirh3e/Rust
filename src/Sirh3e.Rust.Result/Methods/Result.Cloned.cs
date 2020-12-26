9776f38465b77a5eac2ccd4c72c47a758bd3f55c
sirh3e <sirh3e@protonmail.com>
2020-12-26 02:48:53 +0100

    added new methods to implement

--- src/Sirh3e.Rust.Result/Methods/Result.Cloned.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.Cloned.cs
@@ -4,7 +4,7 @@ namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
-        public Result<TOk, TErr> Cloned() =>
+        public Result<TOk, TErr> Cloned() => //ToDo refactor this view: https://doc.rust-lang.org/std/result/enum.Result.html#impl-Clone
             IsOk ? Ok(_ok) : Err(_err);
     }
 }
