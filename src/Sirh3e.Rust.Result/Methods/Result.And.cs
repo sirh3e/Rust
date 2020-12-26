5d1574c4e6b5427dda1de0aee87a5f5d84017446
sirh3e <sirh3e@protonmail.com>
2020-12-26 20:51:47 +0100

    Added Summary for Result.And.cs renamed U into T matches c# guide lines better

--- src/Sirh3e.Rust.Result/Methods/Result.And.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.And.cs
@@ -2,17 +2,23 @@
 {
     public readonly partial struct Result<TOk, TErr>
     {
-        public Result<U, TErr> And<U>(Result<U, TErr> res)
+        /// <summary>
+        /// Returns res if the result is Ok, otherwise returns the Err value of self
+        /// </summary>
+        /// <param name="res"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>Returns res if the result is Ok, otherwise returns the Err value of self</returns>
+        public Result<T, TErr> And<T>(Result<T, TErr> res)
         {
             if (IsOk && res.IsErr)
             {
-                return Result<U, TErr>.Err(res._err);
+                return Result<T, TErr>.Err(res._err);
             }
 
             return IsErr switch
             {
-                true when res.IsOk => Result<U, TErr>.Err(_err),
-                true when res.IsErr => Result<U, TErr>.Err(_err),
+                true when res.IsOk => Result<T, TErr>.Err(_err),
+                true when res.IsErr => Result<T, TErr>.Err(_err),
                 _ => res
             };
         }
