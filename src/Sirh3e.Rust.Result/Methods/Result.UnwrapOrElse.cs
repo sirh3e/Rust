f90002b42e40f413260ae5f412e4404647a164b1
sirh3e <sirh3e@protonmail.com>
2020-12-26 21:58:06 +0100

    Added Summary for Result.UnwrapOrElse.cs added {} on if

--- src/Sirh3e.Rust.Result/Methods/Result.UnwrapOrElse.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.UnwrapOrElse.cs
@@ -4,13 +4,25 @@ namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
-        public TOk UnwrapOrElse(Func<TErr, TOk> mapper)
+        /// <summary>
+        /// Returns the contained Ok value or computes it from a closure.
+        /// </summary>
+        /// <param name="map"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Throws only if map is null</exception>
+        public TOk UnwrapOrElse(Func<TErr, TOk> map)
         {
             if (IsOk)
+            {
                 return _ok;
-            if (mapper is null)
-                throw new ArgumentNullException(nameof(mapper));
-            return mapper(_err);
+            }
+
+            if (map is null)
+            {
+                throw new ArgumentNullException(nameof(map));
+            }
+
+            return map(_err);
         }
     }
 }
