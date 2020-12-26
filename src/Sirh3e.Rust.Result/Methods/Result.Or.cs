36889600562cf82c8edebdb1b7ec7cc892712509
sirh3e <sirh3e@protonmail.com>
2020-12-26 21:45:23 +0100

    Added Summary for Result.Or.cs

--- src/Sirh3e.Rust.Result/Methods/Result.Or.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.Or.cs
@@ -2,6 +2,13 @@
 {
     public readonly partial struct Result<TOk, TErr>
     {
-        public Result<TOk, TErr> Or(Result<TOk, TErr> other) => IsOk ? this : other;
+        /// <summary>
+        /// Returns res if the result is Err, otherwise returns the Ok value of self.
+        /// Arguments passed to or are eagerly evaluated; if you are passing the result of a function call, it is recommended to use or_else, which is lazily evaluated.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public Result<TOk, TErr> Or(Result<TOk, TErr> other)
+            => IsOk ? this : other;
     }
 }
