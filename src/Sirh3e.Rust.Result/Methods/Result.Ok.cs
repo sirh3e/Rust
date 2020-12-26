918d5e4c12152977e73b7002f5f78580bed620f1
sirh3e <sirh3e@protonmail.com>
2020-12-26 21:43:32 +0100

    Added Summary for Result.Ok.cs

--- src/Sirh3e.Rust.Result/Methods/Result.Ok.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.Ok.cs
@@ -4,11 +4,16 @@ namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
+        /// <summary>
+        /// Converts from Result&lt;TOk, TErr&gt; to Option&lt;TOk&gt;.
+        /// Converts self into an Option&lt;TOk&gt;, consuming self, and discarding the error, if any.
+        /// </summary>
+        /// <returns></returns>
         public Option<TOk> Ok()
             => IsOk switch
             {
                 true => Option<TOk>.Some(_ok),
-                false => Option<TOk>.None,
+                false => Option<TOk>.None
             };
     }
 }
