377d2c19ede4f16310cae8b5f637617e5c283646
sirh3e <sirh3e@protonmail.com>
2020-12-26 20:56:58 +0100

    Added Summary for Result.Contains.cs

--- src/Sirh3e.Rust.Result/Methods/Result.Contains.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.Contains.cs
@@ -2,6 +2,11 @@
 {
     public readonly partial struct Result<TOk, TErr>
     {
+        /// <summary>
+        /// Returns true if the result is an Ok value containing the given value.
+        /// </summary>
+        /// <param name="ok"></param>
+        /// <returns>Returns true if the result is an Ok value containing the given value.</returns>
         public bool Contains(TOk ok) => IsOk && ok.Equals(_ok);
     }
 }
