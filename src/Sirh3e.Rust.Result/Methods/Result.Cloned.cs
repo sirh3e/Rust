f50d4377258769b48613166f54df86505a8afdee
sirh3e <sirh3e@protonmail.com>
2020-12-26 22:04:58 +0100

    Refactored Result.Cloned.cs

--- src/Sirh3e.Rust.Result/Methods/Result.Cloned.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.Cloned.cs
@@ -4,7 +4,7 @@ namespace Sirh3e.Rust.Result
 {
     public readonly partial struct Result<TOk, TErr>
     {
-        public Result<TOk, TErr> Cloned() => //ToDo refactor this view: https://doc.rust-lang.org/std/result/enum.Result.html#impl-Clone
-            IsOk ? Ok(_ok) : Err(_err);
+        public Result<TOk, TErr> Cloned()
+            => IsOk ? Ok(_ok) : Err(_err); //ToDo refactor this view: https://doc.rust-lang.org/std/result/enum.Result.html#impl-Clone
     }
 }
