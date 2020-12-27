8f72e5f9ab115baeb7078c47759c62786fc0f2be
sirh3e <sirh3e@protonmail.com>
2020-12-28 00:09:15 +0100

    Added Option.UnwrapNone.cs

---
+++ src/Sirh3e.Rust.Option/Methods/Option.UnwrapNone.cs
@@ -0,0 +1,13 @@
+﻿namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public void UnwrapNone()
+        {
+            Match(
+                some => ExpectNoneFailed("called `Option.UnwrapNone()` on a `Some` value", some),
+                _ => { }
+            );
+        }
+    }
+}
