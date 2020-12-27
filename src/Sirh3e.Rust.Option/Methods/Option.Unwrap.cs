bc2f8c60dbf54bbba970c589ff53dacb2f49d5eb
sirh3e <sirh3e@protonmail.com>
2020-12-28 00:00:59 +0100

    Refactored Option.Unwrap.cs

---
+++ src/Sirh3e.Rust.Option/Methods/Option.Unwrap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Sirh3e.Rust.Panic;
+
+namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public TSome Unwrap()
+        {
+            return Unwrap($"called `Option.Unwrap()` on a `None` value of type `{typeof(TSome)}`");
+        }
+
+        public TSome Unwrap(string error)
+        {
+            if (string.IsNullOrEmpty(error))
+                throw new ArgumentNullException(nameof(error));
+
+            return Unwrap(() => error);
+        }
+
+        public TSome Unwrap(Func<string> error)
+        {
+            return Match(
+                some => some,
+                () => throw new PanicException(error() ??
+                                               throw new ArgumentNullException(nameof(error)))
+            );
+        }
+    }
+}
--- src/Sirh3e.Rust.Option/Option.cs
+++ src/Sirh3e.Rust.Option/Option.cs
@@ -24,37 +24,12 @@ namespace Sirh3e.Rust.Option
             return new(some);
         }
 
-        public TSome Unwrap()
-        {
-            return Unwrap(() => $"Cannot unwrap \"None\" of type {typeof(TSome)}.");
-        }
 
         public static implicit operator Option<TSome>(None none)
         {
             return None;
         }
 
-        public TSome Unwrap(Func<string> error)
-        {
-            if (IsSome)
-                return Unwrap(error.Invoke());
-
-            if (error is null)
-                throw new ArgumentNullException(nameof(error));
-
-            throw new NotImplementedException(); //ToDo create own exception
-        }
-
-        public TSome Unwrap(string error)
-        {
-            if (string.IsNullOrEmpty(error))
-                throw new ArgumentNullException(nameof(error));
-
-            if (IsNone)
-                throw new NotImplementedException(error); //ToDo create own exception
-
-            return _some;
-        }
 
         public TSome UnwrapOr(TSome other)
         {
