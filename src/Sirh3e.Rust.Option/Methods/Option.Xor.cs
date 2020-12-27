1ab29d225bba92c24139fe62f957746a87e84530
sirh3e <sirh3e@protonmail.com>
2020-12-28 00:28:02 +0100

    Added Option.Xor.cs

---
+++ src/Sirh3e.Rust.Option/Methods/Option.Xor.cs
@@ -0,0 +1,15 @@
+﻿namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public Option<TSome> Xor(Option<TSome> option)
+        {
+            return (IsSome, option.IsSome) switch
+            {
+                (true, false) => this,
+                (false, true) => option,
+                _ => None,
+            };
+        }
+    }
+}
