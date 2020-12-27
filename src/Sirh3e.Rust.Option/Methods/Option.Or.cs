14b129bdc30fce4e99d32d9df34d1312aa9c665e
sirh3e <sirh3e@protonmail.com>
2020-12-27 23:37:12 +0100

    Added Option.Or.cs

---
+++ src/Sirh3e.Rust.Option/Methods/Option.Or.cs
@@ -0,0 +1,10 @@
+﻿namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public Option<TSome> Or(Option<TSome> option)
+        {
+            return Match(Some, () => option);
+        }
+    }
+}
