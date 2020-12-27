6051575b54de4fbba8796e59602c862a070562ee
sirh3e <sirh3e@protonmail.com>
2020-12-27 21:28:52 +0100

    Added ExpectNone on Option.Expect.None.Failed.cs

---
+++ src/Sirh3e.Rust.Option/Methods/Option.Expect.None.cs
@@ -0,0 +1,8 @@
+﻿namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public void ExpectNone(string message)
+            => Match(some => { }, some => ExpectNoneFailed(message, some));
+    }
+}
--- src/Sirh3e.Rust.Option/Option.Match.cs
+++ src/Sirh3e.Rust.Option/Option.Match.cs
@@ -26,6 +26,28 @@ namespace Sirh3e.Rust.Option
             }
         }
 
+        private void Match(Action<TSome> onSome, Action<TSome> onNone) //ToDo only use of Option.Expect.None.Failed
+        {
+            if (IsSome)
+            {
+                if (onSome is null)
+                {
+                    throw new ArgumentNullException(nameof(onSome));
+                }
+
+                onSome(_some);
+            }
+            else
+            {
+                if (onNone is null)
+                {
+                    throw new ArgumentNullException(nameof(onNone));
+                }
+
+                onNone(_some);
+            }
+        }
+
         public T Match<T>(Func<TSome, T> onSome, Func<T> onNone)
         {
             if (IsSome)
