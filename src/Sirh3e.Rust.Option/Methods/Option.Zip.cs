c41d2af4a85e594e1c1450d2925d11df16a59291
sirh3e <sirh3e@protonmail.com>
2020-12-28 00:30:34 +0100

    Added Option.Zip.cs

---
+++ src/Sirh3e.Rust.Option/Methods/Option.Zip.cs
@@ -0,0 +1,14 @@
+﻿namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public Option<(TSome, T)> Zip<T>(Option<T> option)
+        {
+            return (IsSome, option.IsSome) switch
+            {
+                (true, true) => Option<(TSome, T)>.Some((_some, option._some)),
+                _ => Option<(TSome, T)>.None
+            };
+        }
+    }
+}
