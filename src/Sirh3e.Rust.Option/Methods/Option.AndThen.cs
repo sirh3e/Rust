3faadb7af086554cf12d722525fcf32e5c650c07
sirh3e <sirh3e@protonmail.com>
2020-12-27 03:17:43 +0100

    added AndThen function on Option.cs

--- src/Sirh3e.Rust.Option/Methods/Option.AndThen.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.AndThen.cs
@@ -1,7 +1,10 @@
-﻿namespace Sirh3e.Rust.Option
+﻿using System;
+
+namespace Sirh3e.Rust.Option
 {
     public readonly partial struct Option<TSome>
     {
-
+        public Option<TSome> AndThen(Func<TSome, Option<TSome>> func)
+            => Match(func, () => None);
     }
 }
