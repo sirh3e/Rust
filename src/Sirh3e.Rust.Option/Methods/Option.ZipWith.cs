925c43810120ce24a3a1e854c5717472e536252b
sirh3e <sirh3e@protonmail.com>
2020-12-28 00:36:56 +0100

    Added Option.ZipWith.cs

---
+++ src/Sirh3e.Rust.Option/Methods/Option.ZipWith.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public Option<TR> ZipWith<TU, TR>(Option<TU> option, Func<TSome, TU, TR> func)
+        {
+            return (IsSome, option.IsSome) switch
+            {
+                (true, true) => Option<TR>.Some(func(_some, option._some) ??
+                                                throw new ArgumentNullException(nameof(func))),
+                _ => Option<TR>.None
+            };
+        }
+    }
+}
