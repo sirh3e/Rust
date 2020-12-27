b68ad86d7e80887879fff0af39699b7b10d0fcbc
sirh3e <sirh3e@protonmail.com>
2020-12-27 21:37:33 +0100

    Added Option.Filter

---
+++ src/Sirh3e.Rust.Option/Methods/Option.Filter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public Option<TSome> Filter(Func<TSome, bool> predicate)
+        {
+            if (!IsSome)
+            {
+                return None;
+            }
+
+            if (predicate is null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            return predicate(_some) ? Some(_some) : None;
+        }
+    }
+}
