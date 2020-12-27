9c06a8262faad8867de84201704160ccd9744fa0
sirh3e <sirh3e@protonmail.com>
2020-12-27 02:45:18 +0100

    Added Unstable Attribute

---
+++ src/Sirh3e.Rust.Attributes/Unstable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sirh3e.Rust.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Interface | AttributeTargets.Struct)]
+    public class Unstable : Attribute
+    {
+        public readonly string Feature;
+        public readonly ulong Issue;
+
+        public Unstable(string feature, ulong issue)
+        {
+            Feature = feature ?? throw new ArgumentNullException(nameof(feature));
+            Issue = issue;
+        }
+    }
+}
