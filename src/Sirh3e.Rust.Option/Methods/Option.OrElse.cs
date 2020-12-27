854102e63335fdaeadcccd6ac9f781ba149e6df8
sirh3e <sirh3e@protonmail.com>
2020-12-27 23:40:51 +0100

    Option.OrElse.cs

---
+++ src/Sirh3e.Rust.Option/Methods/Option.OrElse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public Option<TSome> OrElse(Func<Option<TSome>> func)
+        {
+            return Match(Some, func ?? throw new ArgumentNullException(nameof(func)));
+        }
+    }
+}
