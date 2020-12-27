589db0fa0d14f3dbffc87ba239c08831e02f8aaa
sirh3e <sirh3e@protonmail.com>
2020-12-27 21:52:39 +0100

    added GetOrInsert on Option.Get.Or.Insert.With but not implemented

---
+++ src/Sirh3e.Rust.Option/Methods/Option.Get.Or.Insert.With.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public Option<TSome> GetOrInsertWith(Func<TSome> func)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
