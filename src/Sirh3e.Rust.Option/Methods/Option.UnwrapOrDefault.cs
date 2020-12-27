652389ab009a40ff2b525ccf66430262eb9d8fd2
sirh3e <sirh3e@protonmail.com>
2020-12-28 00:17:46 +0100

    Added Option.UnwrapOrDefault

---
+++ src/Sirh3e.Rust.Option/Methods/Option.UnwrapOrDefault.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public TSome UnwrapOrDefault()
+        {
+            return Match(
+                some => some,
+                Activator.CreateInstance<TSome>
+            );
+        }
+    }
+}
