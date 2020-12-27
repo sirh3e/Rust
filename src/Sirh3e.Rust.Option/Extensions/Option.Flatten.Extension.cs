a533c286079742a0f339e45eaf757096325d09c0
sirh3e <sirh3e@protonmail.com>
2020-12-27 21:46:42 +0100

    Added Option.Flatten

---
+++ src/Sirh3e.Rust.Option/Extensions/Option.Flatten.Extension.cs
@@ -0,0 +1,8 @@
+﻿namespace Sirh3e.Rust.Option.Extensions
+{
+    public static partial class OptionExtension
+    {
+        public static Option<TSome> Flatten<TSome>(this Option<Option<TSome>> option)
+            => option.IsNone ? Option<TSome>.None : option.Unwrap();
+    }
+}
