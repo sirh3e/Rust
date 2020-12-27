88e3325ac6d2b09a1798b2b825b28cc0e210ad2a
sirh3e <sirh3e@protonmail.com>
2020-12-27 03:21:07 +0100

    added Contains function on Option.cs

---
+++ src/Sirh3e.Rust.Option/Methods/Option.Contains.cs
@@ -0,0 +1,8 @@
+﻿namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        public bool Contains(TSome some)
+            => Match(s => s.Equals(some), () => false);
+    }
+}
