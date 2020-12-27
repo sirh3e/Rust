2985c02d62eb280620ca4322b8b0329cf6457c2a
sirh3e <sirh3e@protonmail.com>
2020-12-28 00:39:40 +0100

    renamed:    src/Sirh3e.Rust.Option/Methods/Option.Expect.None.cs -> src/Sirh3e.Rust.Option/Methods/Option.ExpectNone.cs

--- src/Sirh3e.Rust.Option/Methods/Option.Expect.None.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sirh3e.Rust.Option
-{
-    public readonly partial struct Option<TSome>
-    {
-        public void ExpectNone(string message)
-            => Match(_ => { }, some => ExpectNoneFailed(message, some));
-    }
-}
