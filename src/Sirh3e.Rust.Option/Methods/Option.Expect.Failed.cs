0b4e0f97e58ee3d4a56a053ada5a03f56c1c1470
sirh3e <sirh3e@protonmail.com>
2020-12-27 03:28:47 +0100

    added ExpectFailed

--- src/Sirh3e.Rust.Option/Methods/Option.Expect.Failed.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.Expect.Failed.cs
@@ -1,10 +1,13 @@
-﻿namespace Sirh3e.Rust.Option
+﻿using System;
+using Sirh3e.Rust.Panic;
+
+namespace Sirh3e.Rust.Option
 {
     public readonly partial struct Option<TSome>
     {
         internal void ExpectFailed(string message)
         {
-            
+            throw new PanicException(message ?? throw new ArgumentNullException(nameof(message)));
         }
     }
 }
