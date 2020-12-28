2d1d9c279f916126fe2471cfc962f0f1f6257b46
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:30:11 +0100

    Added summary to Option.UnwrapOrDefault.cs

--- src/Sirh3e.Rust.Option/Methods/Option.UnwrapOrDefault.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.UnwrapOrDefault.cs
@@ -4,6 +4,11 @@ namespace Sirh3e.Rust.Option
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Returns the contained Some value or a default
+        /// Consumes the self argument then, if Some, returns the contained value, otherwise if None, returns the default value for that type.
+        /// </summary>
+        /// <returns></returns>
         public TSome UnwrapOrDefault()
         {
             return Match(
