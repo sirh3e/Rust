4c59e2299b6e88fb4cec981f47a445febe5a44bc
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:29:33 +0100

    Added summary to Option.UnwrapOr.cs

--- src/Sirh3e.Rust.Option/Methods/Option.UnwrapOr.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.UnwrapOr.cs
@@ -4,6 +4,12 @@ namespace Sirh3e.Rust.Option
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Returns the contained Some value or a provided default.
+        /// Arguments passed to unwrap_or are eagerly evaluated; if you are passing the result of a function call, it is recommended to use unwrap_or_else, which is lazily evaluated.
+        /// </summary>
+        /// <param name="default"></param>
+        /// <returns></returns>
         public TSome UnwrapOr(TSome @default)
         {
             return Match(
