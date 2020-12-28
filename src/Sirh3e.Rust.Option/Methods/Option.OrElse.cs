99979be9ebcb8aee62e2556e8c8242e40915a269
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:25:44 +0100

    Added summary to Option.OrElse.cs

--- src/Sirh3e.Rust.Option/Methods/Option.OrElse.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.OrElse.cs
@@ -4,6 +4,11 @@ namespace Sirh3e.Rust.Option
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Returns the option if it contains a value, otherwise calls func and returns the result. 
+        /// </summary>
+        /// <param name="func"></param>
+        /// <returns></returns>
         public Option<TSome> OrElse(Func<Option<TSome>> func)
         {
             return Match(Some, func ?? throw new ArgumentNullException(nameof(func)));
