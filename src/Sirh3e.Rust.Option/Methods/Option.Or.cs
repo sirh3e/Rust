dc86b24708b941cc2d3064d82d0d3e40dce3ad8e
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:24:56 +0100

    Added summary to Option.Or.cs

--- src/Sirh3e.Rust.Option/Methods/Option.Or.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.Or.cs
@@ -2,6 +2,11 @@
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Returns the option if it contains a value, otherwise returns option.
+        /// </summary>
+        /// <param name="option"></param>
+        /// <returns></returns>
         public Option<TSome> Or(Option<TSome> option)
         {
             return Match(Some, () => option);
