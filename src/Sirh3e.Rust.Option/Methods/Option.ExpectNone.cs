6a940e27bdecb76b294d4cc7bb891024f69e03db
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:10:02 +0100

    Added summary to Option.ExpectNone.cs

--- src/Sirh3e.Rust.Option/Methods/Option.ExpectNone.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.ExpectNone.cs
@@ -2,6 +2,10 @@
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Consumes self while expecting None and returning nothing.
+        /// </summary>
+        /// <param name="message"></param>
         public void ExpectNone(string message)
             => Match(_ => { }, some => ExpectNoneFailed(message, some));
     }
