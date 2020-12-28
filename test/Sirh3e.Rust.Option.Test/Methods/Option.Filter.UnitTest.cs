579d01601710c3b4b2c9b99fee20e9539e85a149
sirh3e <sirh3e@protonmail.com>
2020-12-28 06:52:22 +0100

    Added Option.Filter.UnitTest.cs

---
+++ test/Sirh3e.Rust.Option.Test/Methods/Option.Filter.UnitTest.cs
@@ -0,0 +1,38 @@
+using System;
+using FluentAssertions;
+using Sirh3e.Rust.Panic;
+using Xunit;
+
+namespace Sirh3e.Rust.Option.Test
+{
+    public partial class OptionUnitTest
+    {
+        [Fact]
+        public void Option_Filter()
+        {
+            {
+                var x = Option<uint>.Some(42);
+
+                x.IsSome.Should().BeTrue();
+                x.IsNone.Should().BeFalse();
+                
+                Action action = () => x.Expect("fruits are healthy");
+
+                action.Should()
+                    .ThrowExactly<PanicException>()
+                    .WithMessage("fruits are healthy");
+            }
+            
+            {
+                var x = Option<uint>.None;
+
+                x.IsSome.Should().BeFalse();
+                x.IsNone.Should().BeTrue();
+                
+                Action action = () => x.Expect("fruits are healthy");
+
+                action.Should().NotThrow();
+            }
+        }
+    }
+}
