707e836edc597dc12b31322410b2e44396dcf7c4
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:44:22 +0100

    Added Option.And.UnitTest.cs

--- test/Sirh3e.Rust.Option.Test/Methods/Option.And.UnitTest.cs
+++ test/Sirh3e.Rust.Option.Test/Methods/Option.And.UnitTest.cs
@@ -26,6 +26,44 @@ namespace Sirh3e.Rust.Option.Test
                 
                 option.UnwrapNone();
             }
+
+            {
+                var x = Option<uint>.None;
+
+                x.IsSome.Should().BeFalse();
+                x.IsNone.Should().BeTrue();
+                
+                var y = Option<string>.Some("foo");
+
+                y.IsSome.Should().BeTrue();
+                y.IsNone.Should().BeFalse();
+
+                var option = x.And(y);
+                
+                option.IsSome.Should().BeFalse();
+                option.IsNone.Should().BeTrue();
+                
+                option.UnwrapNone();
+            }
+            
+            {
+                var x = Option<uint>.Some(2);
+
+                x.IsSome.Should().BeTrue();
+                x.IsNone.Should().BeFalse();
+                
+                var y = Option<string>.Some("foo");
+
+                y.IsSome.Should().BeTrue();
+                y.IsNone.Should().BeFalse();
+
+                var option = x.And(y);
+                
+                option.IsSome.Should().BeTrue();
+                option.IsNone.Should().BeFalse();
+
+                option.Unwrap().Should().Be("foo");
+            }
         }
     }
 }
