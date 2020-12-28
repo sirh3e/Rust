509694b91002e9f7561e1a71c6d1973da92d2c75
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:28:14 +0100

    Added summary to Option.Unwrap.cs

--- src/Sirh3e.Rust.Option/Methods/Option.Unwrap.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.Unwrap.cs
@@ -5,11 +5,23 @@ namespace Sirh3e.Rust.Option
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Returns the contained Some value, consuming the self value.
+        /// Because this function may panic, its use is generally discouraged. Instead, prefer to use pattern matching and handle the None case explicitly, or call unwrap_or, unwrap_or_else, or unwrap_or_default
+        /// </summary>
+        /// <returns></returns>
         public TSome Unwrap()
         {
             return Unwrap($"called `Option.Unwrap()` on a `None` value of type `{typeof(TSome)}`");
         }
 
+        /// <summary>
+        /// Returns the contained Some value, consuming the self value with custom error message.
+        /// Because this function may panic, its use is generally discouraged. Instead, prefer to use pattern matching and handle the None case explicitly, or call unwrap_or, unwrap_or_else, or unwrap_or_default
+        /// </summary>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Throws only if error is null or empty</exception>
         public TSome Unwrap(string error)
         {
             if (string.IsNullOrEmpty(error))
@@ -18,6 +30,12 @@ namespace Sirh3e.Rust.Option
             return Unwrap(() => error);
         }
 
+        /// <summary>
+        /// Returns the contained Some value, consuming the self value with custom error message.
+        /// Because this function may panic, its use is generally discouraged. Instead, prefer to use pattern matching and handle the None case explicitly, or call unwrap_or, unwrap_or_else, or unwrap_or_default
+        /// </summary>
+        /// <param name="error"></param>
+        /// <returns></returns>
         public TSome Unwrap(Func<string> error)
         {
             return Match(
