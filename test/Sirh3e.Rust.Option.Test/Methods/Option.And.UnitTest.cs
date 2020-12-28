72bc2a61f428f0abd860730ee68e2c5edf413536
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:42:18 +0100

    Added Option.And.UnitTest.cs

--- test/Sirh3e.Rust.Option.Test/Methods/Option.And.UnitTest.cs
+++ test/Sirh3e.Rust.Option.Test/Methods/Option.And.UnitTest.cs
@@ -1,7 +1,31 @@
-﻿namespace Sirh3e.Rust.Option.Test
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Sirh3e.Rust.Option.Test
 {
     public partial class OptionUnitTest
     {
-        
+        [Fact]
+        public void Option_And()
+        {
+            {
+                var x = Option<uint>.Some(2);
+
+                x.IsSome.Should().BeTrue();
+                x.IsNone.Should().BeFalse();
+                
+                var y = Option<string>.None;
+
+                y.IsSome.Should().BeFalse();
+                y.IsNone.Should().BeTrue();
+
+                var option = x.And(y);
+                
+                option.IsSome.Should().BeFalse();
+                option.IsNone.Should().BeTrue();
+                
+                option.UnwrapNone();
+            }
+        }
     }
 }
