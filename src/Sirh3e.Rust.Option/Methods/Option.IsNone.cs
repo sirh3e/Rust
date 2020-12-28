7442187c6c483748c8a8e544b274e568b0bd7818
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:17:01 +0100

    Added summary to Option.IsNone.cs

--- src/Sirh3e.Rust.Option/Methods/Option.IsNone.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.IsNone.cs
@@ -2,6 +2,9 @@
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Returns true if the option is a None value.
+        /// </summary>
         public bool IsNone => IsSome == false;
     }
 }
