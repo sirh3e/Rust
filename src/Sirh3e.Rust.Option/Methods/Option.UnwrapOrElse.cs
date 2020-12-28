20c77fb55728aaa54ddfa42158aee7ae1a3f58f6
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:30:54 +0100

    Added summary to Option.UnwrapOrElse.cs

--- src/Sirh3e.Rust.Option/Methods/Option.UnwrapOrElse.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.UnwrapOrElse.cs
@@ -4,6 +4,11 @@ namespace Sirh3e.Rust.Option
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Returns the contained Some value or computes it from a Func.
+        /// </summary>
+        /// <param name="func"></param>
+        /// <returns></returns>
         public TSome UnwrapOrElse(Func<TSome> func)
         {
             return Match(some => some, func ?? throw new ArgumentNullException(nameof(func)));
