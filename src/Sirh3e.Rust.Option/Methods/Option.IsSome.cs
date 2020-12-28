aeb51fe34825d25ae53a529e6ae852f689f6475f
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:17:26 +0100

    Added summary to Option.IsSome.cs

--- src/Sirh3e.Rust.Option/Methods/Option.IsSome.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.IsSome.cs
@@ -2,6 +2,9 @@
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Returns true if the option is a Some value.
+        /// </summary>
         public readonly bool IsSome;
     }
 }
