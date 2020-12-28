a4d0dc4e915cb799a1f4bb72a51bef6c786fc189
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:31:39 +0100

    Added summary to Option.Xor.cs

--- src/Sirh3e.Rust.Option/Methods/Option.Xor.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.Xor.cs
@@ -2,6 +2,11 @@
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Returns Some if exactly one of self, option is Some, otherwise returns None.
+        /// </summary>
+        /// <param name="option"></param>
+        /// <returns></returns>
         public Option<TSome> Xor(Option<TSome> option)
         {
             return (IsSome, option.IsSome) switch
