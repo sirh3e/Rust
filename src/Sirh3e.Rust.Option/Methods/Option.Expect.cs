204e4aa1e8aa57eeb0348b8eb4d6086b3c39922b
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:09:17 +0100

    Added summary to Option.Expect.cs

--- src/Sirh3e.Rust.Option/Methods/Option.Expect.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.Expect.cs
@@ -2,6 +2,11 @@
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Returns true if the option is a Some value containing the given value.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public TSome Expect(string message)
             => Match(s => s, () => ExpectFailed(message));
     }
