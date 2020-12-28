b90bd0ba62d6fd5267c2810fa2daf6567e2b6058
sirh3e <sirh3e@protonmail.com>
2020-12-28 03:18:48 +0100

    Added summary to Option.Map.cs

--- src/Sirh3e.Rust.Option/Methods/Option.Map.cs
+++ src/Sirh3e.Rust.Option/Methods/Option.Map.cs
@@ -4,6 +4,12 @@ namespace Sirh3e.Rust.Option
 {
     public readonly partial struct Option<TSome>
     {
+        /// <summary>
+        /// Maps an Option&lt;TSome&gt; to Option&lt;T&gt; by applying a function to a contained value.
+        /// </summary>
+        /// <param name="map"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public Option<T> Map<T>(Func<TSome, T> map)
         {
             return Match(
