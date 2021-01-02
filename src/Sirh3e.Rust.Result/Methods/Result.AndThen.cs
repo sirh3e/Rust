734b352a97c023d9d05e0f296298fc12397454b2
sirh3e <sirh3e@protonmail.com>
2021-01-02 12:53:56 +0100

    refactored Result.AndThen.cs

--- src/Sirh3e.Rust.Result/Methods/Result.AndThen.cs
+++ src/Sirh3e.Rust.Result/Methods/Result.AndThen.cs
@@ -5,8 +5,8 @@ namespace Sirh3e.Rust.Result
     public readonly partial struct Result<TOk, TErr>
     {
         /// <summary>
-        /// Calls op if the result is Ok, otherwise returns the Err value of self.
-        /// This function can be used for control flow based on Result values.
+        ///     Calls op if the result is Ok, otherwise returns the Err value of self.
+        ///     This function can be used for control flow based on Result values.
         /// </summary>
         /// <param name="op"></param>
         /// <typeparam name="T"></typeparam>
@@ -14,16 +14,14 @@ namespace Sirh3e.Rust.Result
         /// <exception cref="ArgumentNullException">Throws only if op is null</exception>
         public Result<T, TErr> AndThen<T>(Func<TOk, Result<T, TErr>> op)
         {
-            if (!IsOk)
-            {
-                return Result<T, TErr>.Err(_err);
-            }
-
-            if (op is null)
-            {
-                throw new ArgumentNullException(nameof(op));
-            }
-            return op(_ok);
+            return Match(ok =>
+                {
+                    if (op is null)
+                        throw new ArgumentNullException(nameof(op));
+                    return op(ok);
+                },
+                Result<T, TErr>.Err
+            );
         }
     }
 }
