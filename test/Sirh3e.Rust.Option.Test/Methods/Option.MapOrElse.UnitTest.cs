706f1db500f069fd677d0d0170bc1dd1a05a2421
sirh3e <sirh3e@protonmail.com>
2021-01-03 04:43:21 +0100

    refactored Option.MapOrElse.cs into own file

---
+++ test/Sirh3e.Rust.Option.Test/Methods/Option.MapOrElse.UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Sirh3e.Rust.Option.Test
+{
+    public partial class OptionUnitTest
+    {
+        [Fact]
+        public void Option_MapOrElse()
+        {
+            var k = 21;
+            {
+                var x = Option<string>.Some("foo");
+
+                x.IsSome.Should().BeTrue();
+                x.IsNone.Should().BeFalse();
+
+                x.MapOrElse(() => 2 * k, s => s.Length).Should().Be(3);
+            }
+
+            {
+                var x = Option<string>.None;
+
+                x.IsSome.Should().BeFalse();
+                x.IsNone.Should().BeTrue();
+
+                x.MapOrElse(() => 2 * k, s => s.Length).Should().Be(42);
+            }
+        }
+    }
+}
--- test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
+++ test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
@@ -120,30 +120,5 @@ namespace Sirh3e.Rust.Option.Test
                 option.UnwrapOrElse(() => k * 2).Should().Be(20);
             }
         }
-
-
-
-        [Fact]
-        public void Option_MapOrElse()
-        {
-            var k = 21;
-            {
-                var x = Option<string>.Some("foo");
-
-                x.IsSome.Should().BeTrue();
-                x.IsNone.Should().BeFalse();
-
-                x.MapOrElse(() => 2 * k, s => s.Length).Should().Be(3);
-            }
-
-            {
-                var x = Option<string>.None;
-
-                x.IsSome.Should().BeFalse();
-                x.IsNone.Should().BeTrue();
-
-                x.MapOrElse(() => 2 * k, s => s.Length).Should().Be(42);
-            }
-        }
     }
 }
