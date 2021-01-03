9adeb8b448aa7f7b94ed3b65ec2e1392916a9067
sirh3e <sirh3e@protonmail.com>
2021-01-03 04:55:47 +0100

    added test for Option.OkOrElse.UnitTest.cs

---
+++ test/Sirh3e.Rust.Option.Test/Methods/Option.OkOrElse.UnitTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Sirh3e.Rust.Result;
+using Xunit;
+
+namespace Sirh3e.Rust.Option.Test
+{
+    public partial class OptionUnitTest
+    {
+        [Fact]
+        public void Option_OkOrElse()
+        {
+            {
+                var x = Option<string>.Some("foo");
+
+                x.IsSome.Should().BeTrue();
+                x.IsNone.Should().BeFalse();
+
+                x.Unwrap().Should().Be("foo");
+
+                var result = x.OkOrElse(() => 0);
+
+                result.IsOk.Should().BeTrue();
+                result.IsErr.Should().BeFalse();
+
+                result.Unwrap().Should().Be("foo");
+            }
+
+            {
+                var x = Option<string>.None;
+
+                x.IsSome.Should().BeFalse();
+                x.IsNone.Should().BeTrue();
+
+                var result = x.OkOrElse(() => 0);
+
+                result.IsOk.Should().BeFalse();
+                result.IsErr.Should().BeTrue();
+
+                result.UnwrapErr().Should().Be(0);
+            }
+        }
+    }
+}
