2bcac756ae75d4ff624fd39b02f13cb877d13465
sirh3e <sirh3e@protonmail.com>
2021-01-03 06:27:25 +0100

    moved function Option_UnwrapOrElse from OptionUnitTest.cs to Option.UnwrapOrElse.cs

--- test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
+++ test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
@@ -52,28 +52,5 @@ namespace Sirh3e.Rust.Option.Test
                 action.Should().ThrowExactly<PanicException>();
             }
         }
-
-        [Fact]
-        public void Option_UnwrapOrElse()
-        {
-            var k = 10;
-            {
-                var option = Option<int>.Some(4);
-
-                option.IsSome.Should().BeTrue();
-                option.IsNone.Should().BeFalse();
-
-                option.UnwrapOrElse(() => k * 2).Should().Be(4);
-            }
-
-            {
-                var option = Option<int>.None; //ToDo
-
-                option.IsSome.Should().BeFalse();
-                option.IsNone.Should().BeTrue();
-
-                option.UnwrapOrElse(() => k * 2).Should().Be(20);
-            }
-        }
     }
 }
