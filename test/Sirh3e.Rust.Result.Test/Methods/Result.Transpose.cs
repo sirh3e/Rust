c822bda490e2a3dddfb5c24943aa8ab5311ec390
sirh3e <sirh3e@protonmail.com>
2021-01-03 12:01:26 +0100

    added more cases to Result.Transpose.UnitTest.cs

--- test/Sirh3e.Rust.Result.Test/Methods/Result.Transpose.cs
+++ test/Sirh3e.Rust.Result.Test/Methods/Result.Transpose.cs
@@ -9,10 +9,26 @@ namespace Sirh3e.Rust.Result.Test
         [Fact]
         public void Result_Transpose()
         {
-            var x = Result<Option<int>, string>.Ok(Option<int>.Some(5));
-            var y = Option<Result<int, string>>.Some(Result<int, string>.Ok(5));
+            {
+                var x = Result<Option<int>, string>.Ok(Option<int>.Some(5));
+                var y = Option<Result<int, string>>.Some(Result<int, string>.Ok(5));
 
-            x.Transpose().Equals(y).Should().BeTrue();
+                x.Transpose().Equals(y).Should().BeTrue();
+            }
+
+            {
+                var x = Result<Option<int>, string>.Ok(Option<int>.None);
+                var y = Option<Result<int, string>>.None;
+
+                x.Transpose().Equals(y).Should().BeTrue();
+            }
+
+            {
+                var x = Result<Option<int>, string>.Err("Panic!!!!!!");
+                var y = Option<Result<int, string>>.Some(Result<int, string>.Err("Panic!!!!!!"));
+
+                x.Transpose().Equals(y).Should().BeTrue();
+            }
         }
     }
 }
