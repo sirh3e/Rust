fde4740f1622d955a15e5ff8885ed515cd2d6485
sirh3e <sirh3e@protonmail.com>
2021-01-03 06:15:40 +0100

    moved to Option.UnwrapOr.UnitTest.cs

---
+++ test/Sirh3e.Rust.Option.Test/Methods/Option.UnwrapOr.UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Sirh3e.Rust.Option.Test
+{
+    public partial class OptionUnitTest
+    {
+        [Fact]
+        public void Option_UnwrapOr()
+        {
+            {
+                var option = Option<string>.Some("car");
+
+                option.IsSome.Should().BeTrue();
+                option.IsNone.Should().BeFalse();
+
+                option.UnwrapOr("bike").Should().Be("car");
+            }
+
+            {
+                var option = Option<string>.Some(null);
+
+                option.IsSome.Should().BeFalse();
+                option.IsNone.Should().BeTrue();
+
+                option.UnwrapOr("bike").Should().Be("bike");
+            }
+        }
+    }
+}
--- test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
+++ test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
@@ -53,29 +53,6 @@ namespace Sirh3e.Rust.Option.Test
             }
         }
 
-
-        [Fact]
-        public void Option_UnwrapOr()
-        {
-            {
-                var option = Option<string>.Some("car");
-
-                option.IsSome.Should().BeTrue();
-                option.IsNone.Should().BeFalse();
-
-                option.UnwrapOr("bike").Should().Be("car");
-            }
-
-            {
-                var option = Option<string>.Some(null);
-
-                option.IsSome.Should().BeFalse();
-                option.IsNone.Should().BeTrue();
-
-                option.UnwrapOr("bike").Should().Be("bike");
-            }
-        }
-
         [Fact]
         public void Option_UnwrapOrElse()
         {
