dd1d47456bcd3c647153164fa389fde416aede57
sirh3e <sirh3e@protonmail.com>
2021-01-03 04:15:14 +0100

    Removed empty files

--- src/Sirh3e.Rust/Result/Methods/Result.Flatten.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sirh3e.Rust.Result
-{
-    public readonly partial struct Result<TOk, TErr>
-    {
-        //Have a lock to Sirh3e.Rust.Result.Extensions.ResultExtension
-        //File: ./src/Sirh3e.Rust.Result/Extensions/Result.Flatten.Extension.cs
-    }
-}
