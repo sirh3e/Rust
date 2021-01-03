9d0ae8d721cd03c08feb4de578f1ae6f106b7f06
sirh3e <sirh3e@protonmail.com>
2021-01-03 05:37:41 +0100

    moved function Unwrap from Option.UnitTest.cs -> Option.Unwrap.UnitTest.cs

---
+++ test/Sirh3e.Rust.Option.Test/Methods/Option.Unwrap.UnitTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentAssertions;
+using Sirh3e.Rust.Panic;
+using Xunit;
+
+namespace Sirh3e.Rust.Option.Test
+{
+    public partial class OptionUnitTest
+    {
+        [Fact]
+        public void Option_Unwrap()
+        {
+            {
+                var option = Option<string>.Some("air");
+
+                option.IsSome.Should().BeTrue();
+                option.IsNone.Should().BeFalse();
+
+                option.Unwrap().Should().Be("air");
+            }
+
+            {
+                var option = Option<string>.None;
+
+                option.IsSome.Should().BeFalse();
+                option.IsNone.Should().BeTrue();
+
+                Action action = () => option.Unwrap();
+                action.Should().ThrowExactly<PanicException>();
+            }
+        }
+    }
+}
--- test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
+++ test/Sirh3e.Rust.Option.Test/OptionUnitTest.cs
@@ -53,28 +53,6 @@ namespace Sirh3e.Rust.Option.Test
             }
         }
 
-        [Fact]
-        public void Option_Unwrap()
-        {
-            {
-                var option = Option<string>.Some("air");
-
-                option.IsSome.Should().BeTrue();
-                option.IsNone.Should().BeFalse();
-
-                option.Unwrap().Should().Be("air");
-            }
-
-            {
-                var option = Option<string>.None;
-
-                option.IsSome.Should().BeFalse();
-                option.IsNone.Should().BeTrue();
-
-                Action action = () => option.Unwrap();
-                action.Should().ThrowExactly<PanicException>();
-            }
-        }
 
         [Fact]
         public void Option_UnwrapOr()
