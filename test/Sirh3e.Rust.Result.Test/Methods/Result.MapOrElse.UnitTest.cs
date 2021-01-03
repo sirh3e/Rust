81f65289db5600852dc8bd6339f2ad6bbcf98e11
sirh3e <sirh3e@protonmail.com>
2021-01-03 12:05:07 +0100

    null checks to Result.MapOrElse.UnitTest.cs

--- test/Sirh3e.Rust.Result.Test/Methods/Result.MapOrElse.UnitTest.cs
+++ test/Sirh3e.Rust.Result.Test/Methods/Result.MapOrElse.UnitTest.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Xunit;
 
 namespace Sirh3e.Rust.Result.Test
@@ -25,6 +26,32 @@ namespace Sirh3e.Rust.Result.Test
                 result.IsErr.Should().BeTrue();
 
                 result.MapOrElse(s => k * 2, s => s.Length).Should().Be(42);
+            }
+
+            {
+                var result = Result<string, string>.Ok("foo");
+
+                result.IsOk.Should().BeTrue();
+                result.IsErr.Should().BeFalse();
+
+                Func<string, int> map = null;
+
+                Action action = () => result.MapOrElse(s => k * 2, map);
+
+                action.Should().ThrowExactly<ArgumentNullException>();
+            }
+
+            {
+                var result = Result<string, string>.Err("foo");
+
+                result.IsOk.Should().BeFalse();
+                result.IsErr.Should().BeTrue();
+
+                Func<string, int> @default = null;
+
+                Action action = () => result.MapOrElse(@default, _ => throw new NotImplementedException());
+
+                action.Should().ThrowExactly<ArgumentNullException>();
             }
         }
     }
