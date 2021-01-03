ea3ff356e2709802b142bd2a2da78f3b5ae99515
sirh3e <sirh3e@protonmail.com>
2021-01-03 05:32:58 +0100

    added test Option.Transpose.UnitTest.cs

---
+++ test/Sirh3e.Rust.Option.Test/Methods/Option.Transpose.Extension.UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Sirh3e.Rust.Result;
+using Xunit;
+
+namespace Sirh3e.Rust.Option.Test
+{
+    public partial class OptionUnitTest
+    {
+        struct SomeErr { }
+
+        [Fact]
+        public void Option_Transpose()
+        {
+
+            var x = Result<Option<int>, SomeErr>.Ok(Option<int>.Some(5));
+            var y = Option<Result<int, SomeErr>>.Some(Result<int, SomeErr>.Ok(5));
+
+            x.Should().BeEquivalentTo(y.Transpose());
+        }
+    }
+}
