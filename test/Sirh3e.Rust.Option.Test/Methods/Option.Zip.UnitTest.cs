721ac4013bf5b60a2e319dc811d4b2435dc94b97
sirh3e <sirh3e@protonmail.com>
2021-01-03 06:47:34 +0100

    added test Option.Zip.UnitTest.cs

---
+++ test/Sirh3e.Rust.Option.Test/Methods/Option.Zip.UnitTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Sirh3e.Rust.Option.Test
+{
+    public partial class OptionUnitTest
+    {
+        [Fact]
+        public void Option_Zip()
+        {
+            var x = Option<uint>.Some(1);
+
+            x.IsSome.Should().BeTrue();
+            x.IsNone.Should().BeFalse();
+
+            var y = Option<string>.Some("hi");
+
+            y.IsSome.Should().BeTrue();
+            y.IsNone.Should().BeFalse();
+
+            var z = Option<byte>.None;
+
+            z.IsSome.Should().BeFalse();
+            z.IsNone.Should().BeTrue();
+
+            var option = x.Zip(y);
+
+            option.Should().BeEquivalentTo(Option<(uint, string)>.Some((1, "hi")));
+            x.Zip(y).Should().BeEquivalentTo(Option<(uint, string)>.None);
+        }
+    }
+}
