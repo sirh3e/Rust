3b63063ea79323650b06c027d87bf677d213b405
sirh3e <sirh3e@protonmail.com>
2021-01-03 11:33:35 +0100

    added None.UnitTest.cs

---
+++ test/Sirh3e.Rust.Option.Test/None.UnitTest.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Xunit;
+using Sirh3e.Rust.Option;
+
+namespace Sirh3e.Rust.Option.Test
+{
+    public class NoneUnitTest 
+    {
+        [Fact]
+        public void None_Equals()
+        {
+            var x = None.Value;
+            var y = None.Value;
+
+            x.Equals(y).Should().BeTrue();
+        }
+        
+        [Fact]
+        public void None_Equals_Object()
+        {
+            var x = None.Value;
+            var y = None.Value;
+
+            x.Equals(y as object).Should().BeTrue();
+        }
+        
+        [Fact]
+        public void None_GetHashCode()
+        {
+            var x = None.Value;
+            var y = None.Value;
+
+            x.GetHashCode().Should().Be(y.GetHashCode());
+        }
+        
+        [Fact]
+        public void None_Operator_Equals()
+        {
+            var x = None.Value;
+            var y = None.Value;
+
+            (x == y).Should().BeTrue();
+        }
+        
+        [Fact]
+        public void None_Operator_Not_Equals()
+        {
+            var x = None.Value;
+            var y = None.Value;
+
+            (x != y).Should().BeFalse();
+        }
+    }
+}
