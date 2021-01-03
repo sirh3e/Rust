6dcde65903d43b51aaf942650ab3c8b237ada747
sirh3e <sirh3e@protonmail.com>
2021-01-03 05:00:24 +0100

    added test for Option.Or.UnitTest.cs

---
+++ test/Sirh3e.Rust.Option.Test/Methods/Option.Or.UnitTest.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using Sirh3e.Rust.Result;
+using Xunit;
+
+namespace Sirh3e.Rust.Option.Test
+{
+    public partial class OptionUnitTest
+    {
+        [Fact]
+        public void Option_Or()
+        {
+            {
+                var x = Option<int>.Some(2);
+
+                x.IsSome.Should().BeTrue();
+                x.IsNone.Should().BeFalse();
+
+                x.Unwrap().Should().Be(2);
+
+                var y = Option<int>.None;
+
+                y.IsSome.Should().BeFalse();
+                y.IsNone.Should().BeTrue();
+
+                var option = x.Or(y);
+
+                option.IsSome.Should().BeTrue();
+                option.IsNone.Should().BeFalse();
+
+                option.Unwrap().Should().Be(2);
+            }
+
+            {
+                var x = Option<int>.None;
+
+                x.IsSome.Should().BeFalse();
+                x.IsNone.Should().BeTrue();
+
+                var y = Option<int>.Some(100);
+
+                y.IsSome.Should().BeTrue();
+                y.IsNone.Should().BeFalse();
+
+                y.Unwrap().Should().Be(100);
+
+                var option = x.Or(y);
+
+                option.IsSome.Should().BeTrue();
+                option.IsNone.Should().BeFalse();
+
+                option.Unwrap().Should().Be(100);
+            }
+        }
+    }
+}
