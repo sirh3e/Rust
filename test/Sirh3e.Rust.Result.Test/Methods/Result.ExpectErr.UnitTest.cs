768e1959515a5f1dbb81eebc69bfdf04a5b07293
sirh3e <sirh3e@protonmail.com>
2021-01-03 09:22:20 +0100

    updated Result.ExpectErr.UnitTest.cs

--- test/Sirh3e.Rust.Result.Test/Methods/Result.ExpectErr.UnitTest.cs
+++ test/Sirh3e.Rust.Result.Test/Methods/Result.ExpectErr.UnitTest.cs
@@ -1,7 +1,30 @@
-﻿namespace Sirh3e.Rust.Result.Test
+﻿using System;
+using FluentAssertions;
+using Sirh3e.Rust.Panic;
+using Xunit;
+
+namespace Sirh3e.Rust.Result.Test
 {
     public partial class ResultUnitTest
     {
+        [Fact]
+        public void Result_ExpectErr()
+        {
+            {
+                var x = Result<uint, string>.Err("emergency failure");
 
+                Action action = () => x.ExpectErr("Testing expect");
+
+                action.Should().ThrowExactly<PanicException>();
+            }
+
+            {
+                var x = Result<uint, string>.Ok(42);
+
+                Action action = () => x.ExpectErr(null);
+
+                action.Should().ThrowExactly<ArgumentNullException>();
+            }
+        }
     }
 }
