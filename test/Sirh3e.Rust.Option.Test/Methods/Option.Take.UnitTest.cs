80bdf280513552db427f82a0d0ce8385fab6989b
sirh3e <sirh3e@protonmail.com>
2021-01-03 05:21:53 +0100

    added test Option.Take.UnitTest.cs

---
+++ test/Sirh3e.Rust.Option.Test/Methods/Option.Take.UnitTest.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace Sirh3e.Rust.Option.Test
+{
+    public partial class OptionUnitTest
+    {
+        [Fact]
+        public void Option_Take()
+        {
+            /*
+            {
+                var x = Option<int>.Some(2);
+
+                x.IsSome.Should().BeTrue();
+                x.IsNone.Should().BeFalse();
+
+                x.Unwrap().Should().Be(2);
+
+                var y = x.Take();
+
+                x.IsSome.Should().BeFalse();
+                x.IsNone.Should().BeTrue();
+
+                y.IsSome.Should().BeTrue();
+                y.IsNone.Should().BeFalse();
+
+                y.Unwrap().Should().Be(2);
+            }
+
+            {
+                var x = Option<int>.None;
+
+                x.IsSome.Should().BeFalse();
+                x.IsNone.Should().BeTrue();
+
+                var y = x.Take();
+
+                x.IsSome.Should().BeFalse();
+                x.IsNone.Should().BeTrue();
+
+                y.IsSome.Should().BeFalse();
+                y.IsNone.Should().BeTrue();
+            }
+            */
+        }
+    }
+}
