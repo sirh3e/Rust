0af436fbe7bbc9da5b4d3c8c2296faf990e305a7
sirh3e <sirh3e@protonmail.com>
2021-01-09 23:52:53 +0100

    added Option.Insert.cs

---
+++ src/Sirh3e.Rust/Option/Methods/Option.Insert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sirh3e.Rust.Option
+{
+    public readonly partial struct Option<TSome>
+    {
+        /// <summary>
+        /// Inserts value into the option.
+        /// If the option already contains a value, the old value is dropped.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public TSome Insert(TSome value)
+        {
+            throw new NotImplementedException();
+
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
+        } 
+    }
+}
