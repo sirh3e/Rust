ba3bf79f45c1216bc8ee661f7afe423db7507b43
sirh3e <sirh3e@protonmail.com>
2021-04-09 14:08:30 +0200

    [#3] [ADD] Option.Insert.UnitTest.cs

---
+++ test/Sirh3e.Rust.Test/Option/Methods/Option.Insert.UnitTest.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Sirh3e.Rust.Option;
+using Xunit;
+
+namespace Sirh3e.Rust.Test.Option
+{
+    public partial class OptionUnitTest
+    {
+        [Fact]
+        public void Option_Insert()
+        {
+            var option = Option<int>.None;
+
+            option.IsNone.Should().BeTrue();
+            option.IsSome.Should().BeFalse();
+
+            var value = option.Insert(1);
+            
+            option.IsNone.Should().BeFalse();
+            option.IsSome.Should().BeTrue();
+
+            option.Unwrap().Should().Be(1);
+            value.Should().Be(1);
+
+            value = option.Insert(2);
+            
+            option.IsNone.Should().BeFalse();
+            option.IsSome.Should().BeTrue();
+
+            option.Unwrap().Should().Be(2);
+            value.Should().Be(2);
+            
+            value = option.Insert(3);
+            
+            option.IsNone.Should().BeFalse();
+            option.IsSome.Should().BeTrue();
+
+            option.Unwrap().Should().Be(3);
+            value.Should().Be(3);
+        }
+    }
+}
