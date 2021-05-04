21b6f837acddd92c2aea4d04e1cba00e6a9809b8
sirh3e <marvin.huber@bluewin.ch>
2021-05-04 04:40:54 +0200

    [MODIFIED] applied dotnet format

--- src/Sirh3e.Rust/Result/Result.Ok.cs
+++ src/Sirh3e.Rust/Result/Result.Ok.cs
@@ -5,7 +5,7 @@ namespace Sirh3e.Rust.Result
     public readonly ref struct Ok<TOk>
     {
         private readonly TOk _ok;
-        
+
         public Ok(TOk ok)
         {
             _ok = ok ?? throw new ArgumentNullException(nameof(ok));
