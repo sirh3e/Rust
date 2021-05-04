f9ef5c0e778a7d5e1c2c7b8cabf67155a24fc93f
sirh3e <marvin.huber@bluewin.ch>
2021-05-04 04:36:35 +0200

    [ADDED] Result.Ok.cs for nicer ok on result

---
+++ src/Sirh3e.Rust/Result/Result.Ok.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sirh3e.Rust.Result
+{
+    public readonly ref struct Ok<TOk>
+    {
+        private readonly TOk _ok;
+        
+        public Ok(TOk ok)
+        {
+            _ok = ok ?? throw new ArgumentNullException(nameof(ok));
+        }
+    }
+}
