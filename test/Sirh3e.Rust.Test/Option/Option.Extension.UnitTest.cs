10dc1e432ed4c0bf743eb61e37c988cc31bcede0
sirh3e <marvin.huber@bluewin.ch>
2021-05-04 05:07:42 +0200

    [ADDED] test/Sirh3e.Rust.Test/Option/Option.Extension.UnitTest.cs added unit tests

---
+++ test/Sirh3e.Rust.Test/Option/Option.Extension.UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using static Sirh3e.Rust.Option.Extension;
+using Xunit;
+
+namespace Sirh3e.Rust.Test.Option
+{
+    public partial class OptionUnitTest
+    {
+        [Fact]
+        public void Option_Extension_Construct_Some_FromValue()
+        {
+            {
+                var option = Some("liegens");
+
+                option.IsSome.Should().BeTrue();
+                option.IsNone.Should().BeFalse();
+
+                option.Unwrap().Should().Be("liegens");
+            }
+
+            {
+                var option = Some(42);
+
+                option.IsSome.Should().BeTrue();
+                option.IsNone.Should().BeFalse();
+
+                option.Unwrap().Should().Be(42);
+            }
+        }
+    }
+}
