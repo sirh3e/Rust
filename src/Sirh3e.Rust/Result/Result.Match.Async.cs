920c1c198785e0c4d0ead4b8e9854a06969616dd
sirh3e <marvin.bluewin@bluewin.ch>
2022-03-10 21:03:30 +0100

    [ADDED] support for Result.Match.Async

---
+++ src/Sirh3e.Rust/Result/Result.Match.Async.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Sirh3e.Rust.Result;
+
+public readonly partial struct Result<TOk, TErr>
+{
+    public async Task<T> MatchAsync<T>(Func<TOk, Task<T>> onOk, Func<TErr, Task<T>> onErr)
+        => await Match(async ok => await onOk(ok), async err => await onErr(err));
+
+    public async Task<T> MatchAsync<T>(Func<TOk, T> onOk, Func<TErr, Task<T>> onErr)
+        => await Match(ok => Task.FromResult(onOk(ok)), async err => await onErr(err));
+
+    public async Task<T> MatchAsync<T>(Func<TOk, Task<T>> onOk, Func<TErr, T> onErr)
+        => await Match(async ok => await onOk(ok), err => Task.FromResult(onErr(err)));
+}
