9cf3a785053c8ef4aa9b2a2a72a587b57779a783
sirh3e <marvin.bluewin@bluewin.ch>
2022-03-12 15:41:20 +0100

    added support for ValueTask on Result.MatchAsync

--- src/Sirh3e.Rust/Result/Result.Match.Async.cs
+++ src/Sirh3e.Rust/Result/Result.Match.Async.cs
@@ -8,9 +8,24 @@ public readonly partial struct Result<TOk, TErr>
     public async Task<T> MatchAsync<T>(Func<TOk, Task<T>> onOk, Func<TErr, Task<T>> onErr)
         => await Match(async ok => await onOk(ok), async err => await onErr(err));
 
+    public async Task<T> MatchAsync<T>(Func<TOk, Task<T>> onOk, Func<TErr, ValueTask<T>> onErr)
+        => await Match(async ok => await onOk(ok), async err => await onErr(err));
+
+    public async Task<T> MatchAsync<T>(Func<TOk, ValueTask<T>> onOk, Func<TErr, Task<T>> onErr)
+        => await Match(async ok => await onOk(ok), async err => await onErr(err));
+
+    public async Task<T> MatchAsync<T>(Func<TOk, ValueTask<T>> onOk, Func<TErr, ValueTask<T>> onErr)
+        => await Match(async ok => await onOk(ok), async err => await onErr(err));
+
     public async Task<T> MatchAsync<T>(Func<TOk, T> onOk, Func<TErr, Task<T>> onErr)
         => await Match(ok => Task.FromResult(onOk(ok)), async err => await onErr(err));
 
+    public async Task<T> MatchAsync<T>(Func<TOk, T> onOk, Func<TErr, ValueTask<T>> onErr)
+        => await Match(ok => Task.FromResult(onOk(ok)), async err => await onErr(err));
+
     public async Task<T> MatchAsync<T>(Func<TOk, Task<T>> onOk, Func<TErr, T> onErr)
+        => await Match(async ok => await onOk(ok), err => Task.FromResult(onErr(err)));
+
+    public async Task<T> MatchAsync<T>(Func<TOk, ValueTask<T>> onOk, Func<TErr, T> onErr)
         => await Match(async ok => await onOk(ok), err => Task.FromResult(onErr(err)));
 }
