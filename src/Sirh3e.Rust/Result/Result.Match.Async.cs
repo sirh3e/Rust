38c3be56dcedaeed5665d49ba51a005acfa3332b
sirh3e <marvin.bluewin@bluewin.ch>
2022-03-13 16:14:17 +0100

    removed async on functions

--- src/Sirh3e.Rust/Result/Result.Match.Async.cs
+++ src/Sirh3e.Rust/Result/Result.Match.Async.cs
@@ -5,30 +5,30 @@ namespace Sirh3e.Rust.Result;
 
 public readonly partial struct Result<TOk, TErr>
 {
-    public async Task<T> MatchAsync<T>(Func<TOk, Task<T>> onOk, Func<TErr, Task<T>> onErr)
-        => await Match(async ok => await onOk(ok), async err => await onErr(err));
+    public Task<T> MatchAsync<T>(Func<TOk, Task<T>> onOk, Func<TErr, Task<T>> onErr)
+        => Match(onOk, onErr);
 
-    public async Task<T> MatchAsync<T>(Func<TOk, Task<T>> onOk, Func<TErr, ValueTask<T>> onErr)
-        => await Match(async ok => await onOk(ok), async err => await onErr(err));
+    public Task<T> MatchAsync<T>(Func<TOk, Task<T>> onOk, Func<TErr, ValueTask<T>> onErr)
+        => Match(onOk, err => onErr(err).AsTask());
 
-    public async Task<T> MatchAsync<T>(Func<TOk, ValueTask<T>> onOk, Func<TErr, Task<T>> onErr)
-        => await Match(async ok => await onOk(ok), async err => await onErr(err));
+    public Task<T> MatchAsync<T>(Func<TOk, ValueTask<T>> onOk, Func<TErr, Task<T>> onErr)
+        => Match(ok => onOk(ok).AsTask(), onErr);
 
-    public async Task<T> MatchAsync<T>(Func<TOk, ValueTask<T>> onOk, Func<TErr, ValueTask<T>> onErr)
-        => await Match(async ok => await onOk(ok), async err => await onErr(err));
+    public ValueTask<T> MatchAsync<T>(Func<TOk, ValueTask<T>> onOk, Func<TErr, ValueTask<T>> onErr)
+        => Match(onOk, onErr);
 
-    public async Task<T> MatchAsync<T>(Func<TOk, T> onOk, Func<TErr, Task<T>> onErr)
-        => await Match(ok => Task.FromResult(onOk(ok)), async err => await onErr(err));
+    public Task<T> MatchAsync<T>(Func<TOk, T> onOk, Func<TErr, Task<T>> onErr)
+        => Match(ok => Task.FromResult(onOk(ok)), onErr);
 
-    public async Task<T> MatchAsync<T>(Func<TOk, T> onOk, Func<TErr, ValueTask<T>> onErr)
-        => await Match(ok => Task.FromResult(onOk(ok)), async err => await onErr(err));
+    public ValueTask<T> MatchAsync<T>(Func<TOk, T> onOk, Func<TErr, ValueTask<T>> onErr)
+        => Match(ok => ValueTask.FromResult(onOk(ok)), onErr);
 
-    public async Task<T> MatchAsync<T>(Func<TOk, Task<T>> onOk, Func<TErr, T> onErr)
-        => await Match(async ok => await onOk(ok), err => Task.FromResult(onErr(err)));
+    public Task<T> MatchAsync<T>(Func<TOk, Task<T>> onOk, Func<TErr, T> onErr)
+        => Match(onOk, err => Task.FromResult(onErr(err)));
 
-    public async Task<T> MatchAsync<T>(Func<TOk, ValueTask<T>> onOk, Func<TErr, T> onErr)
-        => await Match(async ok => await onOk(ok), err => Task.FromResult(onErr(err)));
+    public ValueTask<T> MatchAsync<T>(Func<TOk, ValueTask<T>> onOk, Func<TErr, T> onErr)
+        => Match(onOk, err => ValueTask.FromResult(onErr(err)));
 
-    public Task<T> MatchAsync<T>(Func<TOk, T> onOk, Func<TErr, T> onErr)
-        => Task.FromResult(Match(onOk, onErr));
+    public ValueTask<T> MatchAsync<T>(Func<TOk, T> onOk, Func<TErr, T> onErr)
+        => ValueTask.FromResult(Match(onOk, onErr));
 }
