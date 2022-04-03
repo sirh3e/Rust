acee066dad906bd07b02c4040e6b959349067d49
sirh3e <marvin.bluewin@bluewin.ch>
2022-04-03 17:36:22 +0200

    Added Usings.cs for Sirh3e.Rust.Test

---
+++ test/Sirh3e.Rust.Test/Usings.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.Threading.Tasks;
+global using FluentAssertions;
+global using Sirh3e.Rust.Option;
+global using Sirh3e.Rust.Result;
+global using Xunit;
+global using static Sirh3e.Rust.Option.Extension;
+global using static Sirh3e.Rust.Result.Extension;
