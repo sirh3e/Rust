3fd6221b534d1337fa1fbc0815123b08947ebf60
sirh3e <marvin.bluewin@bluewin.ch>
2022-05-01 12:59:31 +0200

    #18 Implemented Result add support for InspectErr

---
+++ src/Sirh3e.Rust/Result/Methods/Result.InspectErr.cs
@@ -0,0 +1,21 @@
+﻿namespace Sirh3e.Rust.Result;
+
+public readonly partial struct Result<TOk, TErr>
+{
+    /// <summary>
+    /// Calls the provided closure with a reference to the contained error (if Err).
+    /// </summary>
+    /// <param name="action"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    [Docs("https://doc.rust-lang.org/std/result/enum.Result.html#method.inspect")]
+    [Unstable("result_option_inspect", 18)]
+    public Result<TOk, TErr> InspectErr(Action<TErr> action)
+    {
+        if ( IsErr )
+        {
+            (action ?? throw new ArgumentNullException(nameof(action)))(_err);
+        }
+        return this;
+    }
+}
