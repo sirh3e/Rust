c6f8fdfdb36387c063cacd1056b4ebfe755442e7
sirh3e <marvin.bluewin@bluewin.ch>
2022-05-01 12:27:55 +0200

    #14 Added attributes: Unstable, Docs

--- src/Sirh3e.Rust/Option/Methods/Option.IsOkWith.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.IsOkWith.cs
@@ -7,6 +7,8 @@ public partial struct Option<TSome>
     /// </summary>
     /// <param name="predicate"></param>
     /// <returns></returns>
+    [Unstable("is_some_with", 21)]
+    [Docs("https://doc.rust-lang.org/std/result/enum.Result.html#method.is_ok_with")]
     public bool IsOkWith(Func<TSome, bool> predicate)
         => Match(predicate, () => false);
 }
