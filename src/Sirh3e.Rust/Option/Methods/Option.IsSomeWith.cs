ce347b65ecc6cfa57da81a9969087aab8214c370
sirh3e <marvin.bluewin@bluewin.ch>
2022-05-01 12:29:41 +0200

    #14 renamed IsOkWith -> IsSomeWith

---
+++ src/Sirh3e.Rust/Option/Methods/Option.IsSomeWith.cs
@@ -0,0 +1,14 @@
+﻿namespace Sirh3e.Rust.Option;
+
+public partial struct Option<TSome>
+{
+    /// <summary>
+    /// Returns true if the option is a Some wrapping a value matching the predicate.
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    [Unstable("is_some_with", 21)]
+    [Docs("https://doc.rust-lang.org/std/result/enum.Result.html#method.is_ok_with")]
+    public bool IsSomeWith(Func<TSome, bool> predicate)
+        => Match(predicate, () => false);
+}
