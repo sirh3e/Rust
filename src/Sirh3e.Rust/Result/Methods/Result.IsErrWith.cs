b4f22fb1a47c1bb18c451df8bf9f12022d85066b
sirh3e <marvin.bluewin@bluewin.ch>
2022-05-01 12:44:02 +0200

    #20 Implemented Result add support for IsErrWith

---
+++ src/Sirh3e.Rust/Result/Methods/Result.IsErrWith.cs
@@ -0,0 +1,14 @@
+﻿namespace Sirh3e.Rust.Result;
+
+public readonly partial struct Result<TOk, TErr>
+{
+    /// <summary>
+    /// Returns true if the result is Err wrapping a value matching the predicate.
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    [Docs("https://doc.rust-lang.org/std/result/enum.Result.html#method.is_err_with")]
+    [Unstable("is_some_with", 20)]
+    public bool IsErrWith(Func<TErr, bool> predicate)
+        => Match(_ => false, predicate);
+}
