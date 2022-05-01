a86318819c828e0c512a8718e9d5717c5c6ccd35
sirh3e <marvin.bluewin@bluewin.ch>
2022-05-01 12:56:20 +0200

    #17 Implemented Result add support for Inspect

---
+++ src/Sirh3e.Rust/Result/Methods/Result.Inspect.cs
@@ -0,0 +1,21 @@
+﻿namespace Sirh3e.Rust.Result;
+
+public readonly partial struct Result<TOk, TErr>
+{
+    /// <summary>
+    /// Calls the provided closure with a reference to the contained value (if Ok).
+    /// </summary>
+    /// <param name="action"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    [Docs("https://doc.rust-lang.org/std/result/enum.Result.html#method.inspect")]
+    [Unstable("result_option_inspect", 17)]
+    public Result<TOk, TErr> Inspect(Action<TOk> action)
+    {
+        if ( IsOk )
+        {
+            (action ?? throw new ArgumentNullException(nameof(action)))(_ok);
+        }
+        return this;
+    }
+}
