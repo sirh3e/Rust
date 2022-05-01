39ae2612d0be56ff0301cbba52dbc7794d1d6a1e
sirh3e <marvin.bluewin@bluewin.ch>
2022-05-01 12:24:33 +0200

    #14 Implemented Result add support for IsOkWith

---
+++ src/Sirh3e.Rust/Option/Methods/Option.IsOkWith.cs
@@ -0,0 +1,12 @@
+﻿namespace Sirh3e.Rust.Option;
+
+public partial struct Option<TSome>
+{
+    /// <summary>
+    /// Returns true if the option is a Some wrapping a value matching the predicate.
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    public bool IsOkWith(Func<TSome, bool> predicate)
+        => Match(predicate, () => false);
+}
