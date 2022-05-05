53bcd545b1f80eefb4fe4a829035a1d5af90ced0
sirh3e <marvin.bluewin@bluewin.ch>
2022-05-05 21:27:18 +0200

    #15 Implemented Option add support for UnwrapUnchecked

---
+++ src/Sirh3e.Rust/Option/Methods/Option.UnwrapUnchecked.cs
@@ -0,0 +1,11 @@
+﻿namespace Sirh3e.Rust.Option;
+
+public partial struct Option<TSome>
+{
+    /// <summary>
+    /// Returns the contained Some value, consuming the self value, without checking that the value is not None.
+    /// </summary>
+    /// <returns></returns>
+    public TSome? UnwrapUnchecked()
+        => Match(some => some, () => default);
+}
