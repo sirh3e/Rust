6fe4dada80b859d4874b86eca047729f67e94e2b
sirh3e <marvin.bluewin@bluewin.ch>
2022-05-05 21:31:32 +0200

    #12 Implemented Option add support for GetOrInsertDefault

---
+++ src/Sirh3e.Rust/Option/Methods/Option.GetOrInsertDefault.cs
@@ -0,0 +1,11 @@
+﻿namespace Sirh3e.Rust.Option;
+
+public partial struct Option<TSome>
+{
+    /// <summary>
+    /// Inserts the default value into the option if it is None, then returns a mutable reference to the contained value.
+    /// </summary>
+    /// <returns></returns>
+    public TSome GetOrInsertDefault()
+        => GetOrInsertWith(() => default);
+}
