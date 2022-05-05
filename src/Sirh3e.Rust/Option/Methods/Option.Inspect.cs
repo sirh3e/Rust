de69e7df2193180249c2a9c1bc8b7b2433a6859f
sirh3e <marvin.bluewin@bluewin.ch>
2022-05-05 21:19:26 +0200

    #13 Implemented Option add support for Inspect

---
+++ src/Sirh3e.Rust/Option/Methods/Option.Inspect.cs
@@ -0,0 +1,18 @@
+﻿namespace Sirh3e.Rust.Option;
+
+public partial struct Option<TSome>
+{
+    /// <summary>
+    /// Calls the provided closure with a reference to the contained value (if Some).
+    /// </summary>
+    /// <param name="inspector"></param>
+    /// <returns></returns>
+    public Option<TSome> Inspect(Action<TSome> inspector)
+    {
+        if ( IsSome )
+        {
+            inspector(_some);
+        }
+        return this;
+    }
+}
