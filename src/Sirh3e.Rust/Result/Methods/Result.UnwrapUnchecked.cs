92228c3cf79bef92bc7953ad44a935c312ce9d2d
sirh3e <sirh3e@protonmail.com>
2022-05-06 08:33:09 +0200

    #23 Implemented Result add support for UnwrapUnchecked

---
+++ src/Sirh3e.Rust/Result/Methods/Result.UnwrapUnchecked.cs
@@ -0,0 +1,11 @@
+namespace Sirh3e.Rust.Result;
+
+public readonly partial struct Result<TOk, TErr>
+{
+    /// <summary>
+    /// Returns the contained Ok value, consuming the self value, without checking that the value is not an Err.
+    /// </summary>
+    /// <returns></returns>
+    public TOk UnwrapUnchecked()
+        => UnwrapOrElse(default);
+}
