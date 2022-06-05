13a0f29ce548409afa4cd4aa405e9459cc2cd8f7
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-05 16:44:33 +0200

    #16 Added attributes to Unzip function

--- src/Sirh3e.Rust/Option/Methods/Option.Unzip.Extension.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.Unzip.Extension.cs
@@ -3,15 +3,18 @@
 public static partial class OptionExtension
 {
     /// <summary>
-    /// Unzips an option containing a tuple of two options.
-    /// If self is Some((a, b)) this method returns (Some(a), Some(b)). Otherwise, (None, None) is returned.
+    ///     Unzips an option containing a tuple of two options.
+    ///     If self is Some((a, b)) this method returns (Some(a), Some(b)). Otherwise, (None, None) is returned.
     /// </summary>
     /// <param name="option"></param>
     /// <typeparam name="TLhs"></typeparam>
     /// <typeparam name="TRhs"></typeparam>
     /// <returns></returns>
+    [Nightly]
+    [GitHub(16, "https://github.com/sirh3e/Rust/issues/16")]
     [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.unzip")]
-    [Unstable("unzip_option", 16)]
+    [Unstable("unzip_option", 87800, "recently added")]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#1724")]
     public static (Option<TLhs>, Option<TRhs>) Unzip<TLhs, TRhs>(this Option<(TLhs, TRhs)> option)
         => option.Match(t => (Option<TLhs>.Some(t.Item1), Option<TRhs>.Some(t.Item2)),
                         () => (Option<TLhs>.None, Option<TRhs>.None));
