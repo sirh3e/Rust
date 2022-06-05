b0a42d4d1b741e453ac481f9a8318257a11b3eb8
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-05 16:46:56 +0200

    #22 Added attributes to UnwrapErrUnchecked function

--- src/Sirh3e.Rust/Result/Methods/Result.UnwrapErrUnchecked.cs
+++ src/Sirh3e.Rust/Result/Methods/Result.UnwrapErrUnchecked.cs
@@ -3,9 +3,13 @@ namespace Sirh3e.Rust.Result;
 public readonly partial struct Result<TOk, TErr>
 {
     /// <summary>
-    /// Returns the contained Err value, consuming the self value, without checking that the value is not an Ok.
+    ///     Returns the contained Err value, consuming the self value, without checking that the value is not an Ok.
     /// </summary>
     /// <returns></returns>
+    [GitHub(22, "https://github.com/sirh3e/Rust/issues/22")]
+    [Docs("https://doc.rust-lang.org/std/result/enum.Result.html#method.unwrap_err_unchecked")]
+    [Stable("option_result_unwrap_unchecked", "1.58.0")]
+    [Source("https://doc.rust-lang.org/src/core/result.rs.html#1523")]
     public TErr UnwrapErrUnchecked()
         => Match(_ => default, err => err);
 }
