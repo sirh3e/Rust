fd588f9e853f77d84a32d08c05dbc3bcb17891c6
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-05 16:44:52 +0200

    #17 Added attributes to Inspect function

--- src/Sirh3e.Rust/Result/Methods/Result.Inspect.cs
+++ src/Sirh3e.Rust/Result/Methods/Result.Inspect.cs
@@ -3,19 +3,20 @@
 public readonly partial struct Result<TOk, TErr>
 {
     /// <summary>
-    /// Calls the provided closure with a reference to the contained value (if Ok).
+    ///     Calls the provided closure with a reference to the contained value (if Ok).
     /// </summary>
     /// <param name="action"></param>
     /// <returns></returns>
     /// <exception cref="ArgumentNullException"></exception>
+    [Nightly]
+    [GitHub(17, "https://github.com/sirh3e/Rust/issues/17")]
     [Docs("https://doc.rust-lang.org/std/result/enum.Result.html#method.inspect")]
-    [Unstable("result_option_inspect", 17)]
+    [Unstable("result_option_inspect", 91345)]
+    [Source("https://doc.rust-lang.org/src/core/result.rs.html#874")]
     public Result<TOk, TErr> Inspect(Action<TOk> action)
     {
         if ( IsOk )
-        {
             (action ?? throw new ArgumentNullException(nameof(action)))(_ok);
-        }
         return this;
     }
 }
