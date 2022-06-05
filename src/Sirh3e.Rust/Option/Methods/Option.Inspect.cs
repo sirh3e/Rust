54d44a9190a58709bb8a7f8f8987832302289726
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-05 16:43:28 +0200

    #14 Added attributes to Inspect function

--- src/Sirh3e.Rust/Option/Methods/Option.Inspect.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.Inspect.cs
@@ -3,16 +3,18 @@
 public partial struct Option<TSome>
 {
     /// <summary>
-    /// Calls the provided closure with a reference to the contained value (if Some).
+    ///     Calls the provided closure with a reference to the contained value (if Some).
     /// </summary>
     /// <param name="inspector"></param>
     /// <returns></returns>
+    [Nightly]
+    [GitHub(13, "https://github.com/sirh3e/Rust/issues/13")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.inspect")]
+    [Unstable("result_option_inspect", 91345)]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#936-939")]
     public Option<TSome> Inspect(Action<TSome> inspector)
     {
-        if ( IsSome )
-        {
-            inspector(_some);
-        }
+        if ( IsSome ) inspector(_some);
         return this;
     }
 }
