5be1eb21b86525bd8d4132388417e20d1569a68e
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-05 16:46:39 +0200

    #20 Added attributes to IsErrAnd function

--- src/Sirh3e.Rust/Result/Methods/Result.IsErrAnd.cs
+++ src/Sirh3e.Rust/Result/Methods/Result.IsErrAnd.cs
@@ -3,12 +3,15 @@
 public readonly partial struct Result<TOk, TErr>
 {
     /// <summary>
-    /// Returns true if the result is Err wrapping a value matching the predicate.
+    ///     Returns true if the result is Err wrapping a value matching the predicate.
     /// </summary>
     /// <param name="predicate"></param>
     /// <returns></returns>
-    [Docs("https://doc.rust-lang.org/std/result/enum.Result.html#method.is_err_with")]
+    [Nightly]
+    [GitHub(20, "https://github.com/sirh3e/Rust/issues/20")]
+    [Docs("https://doc.rust-lang.org/std/result/enum.Result.html#method.is_err_and")]
     [Unstable("is_some_with", 20)]
+    [Source("https://doc.rust-lang.org/src/core/result.rs.html#610")]
     public bool IsErrAnd(Func<TErr, bool> predicate)
         => Match(_ => false, predicate);
 }
