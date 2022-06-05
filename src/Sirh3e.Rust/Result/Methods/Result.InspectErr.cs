f9a93c93fce8606589602124d5daeefff294a077
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-05 16:45:49 +0200

    #18 Added attributes to InspectErr function

--- src/Sirh3e.Rust/Result/Methods/Result.InspectErr.cs
+++ src/Sirh3e.Rust/Result/Methods/Result.InspectErr.cs
@@ -3,19 +3,20 @@
 public readonly partial struct Result<TOk, TErr>
 {
     /// <summary>
-    /// Calls the provided closure with a reference to the contained error (if Err).
+    ///     Calls the provided closure with a reference to the contained error (if Err).
     /// </summary>
     /// <param name="action"></param>
     /// <returns></returns>
     /// <exception cref="ArgumentNullException"></exception>
-    [Docs("https://doc.rust-lang.org/std/result/enum.Result.html#method.inspect")]
-    [Unstable("result_option_inspect", 18)]
+    [Nightly]
+    [GitHub(18, "https://github.com/sirh3e/Rust/issues/18")]
+    [Docs("https://doc.rust-lang.org/std/result/enum.Result.html#method.inspect_err")]
+    [Unstable("result_option_inspect", 91345)]
+    [Source("https://doc.rust-lang.org/src/core/result.rs.html#898")]
     public Result<TOk, TErr> InspectErr(Action<TErr> action)
     {
         if ( IsErr )
-        {
             (action ?? throw new ArgumentNullException(nameof(action)))(_err);
-        }
         return this;
     }
 }
