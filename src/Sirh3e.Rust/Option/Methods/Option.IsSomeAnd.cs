09cbcd7fed671b2bc010e752a6ed40336cd779e6
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-05 16:43:57 +0200

    #14 Added attributes to IsSomeAnd function

--- src/Sirh3e.Rust/Option/Methods/Option.IsSomeAnd.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.IsSomeAnd.cs
@@ -7,8 +7,11 @@ public partial struct Option<TSome>
     /// </summary>
     /// <param name="predicate"></param>
     /// <returns></returns>
-    [Unstable("is_some_with", 14)]
-    [Docs("https://doc.rust-lang.org/std/result/enum.Option.html#method.is_some_and")]
+    [Nightly]
+    [GitHub(14, "https://github.com/sirh3e/Rust/issues/14")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.is_some_and")]
+    [Unstable("is_some_with", 93050)]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#576")]
     public bool IsSomeAnd(Func<TSome, bool> predicate)
         => Match(predicate, () => false);
 }
