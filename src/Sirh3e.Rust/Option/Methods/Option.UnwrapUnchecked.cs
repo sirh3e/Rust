307e7ad4cbd0076cab0ce9546ac5060c1d05282b
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-05 16:44:20 +0200

    #15 Added attributes to UnwrapUnchecked function

--- src/Sirh3e.Rust/Option/Methods/Option.UnwrapUnchecked.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.UnwrapUnchecked.cs
@@ -3,9 +3,14 @@
 public partial struct Option<TSome>
 {
     /// <summary>
-    /// Returns the contained Some value, consuming the self value, without checking that the value is not None.
+    ///     Returns the contained Some value, consuming the self value, without checking that the value is not None.
     /// </summary>
     /// <returns></returns>
+    [Nightly]
+    [GitHub(15, "https://github.com/sirh3e/Rust/issues/15")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.unwrap_unchecked")]
+    [Unstable("const_option_ext", 91930)]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#874")]
     public TSome? UnwrapUnchecked()
         => Match(some => some, () => default);
 }
