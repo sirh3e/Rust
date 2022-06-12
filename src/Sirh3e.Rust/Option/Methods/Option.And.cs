df6f9c7e438beb5180d926aeca4de6f65bb99c87
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-12 13:09:33 +0200

    #24 Option add support for And

--- src/Sirh3e.Rust/Option/Methods/Option.And.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.And.cs
@@ -3,11 +3,17 @@
 public partial struct Option<TSome>
 {
     /// <summary>
-    /// Returns None if the option is None, otherwise returns option.
+    ///     Returns None if the option is None, otherwise returns option.
     /// </summary>
     /// <param name="option"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
+    [GitHub(24, "https://github.com/sirh3e/Rust/issues/24")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.and")]
+    [Stable("rust1", "1.0.0")]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#1203-1206")]
     public Option<T> And<T>(Option<T> option)
-        => IsNone ? Option<T>.None : option;
+        => IsNone
+            ? Option<T>.None
+            : option;
 }
