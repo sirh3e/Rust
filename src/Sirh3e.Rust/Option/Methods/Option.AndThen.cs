54ec3dd4fc4668b03d7ebd38c8c773a225dbe6e5
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-12 13:11:42 +0200

    #25 Option add support for AndThen

--- src/Sirh3e.Rust/Option/Methods/Option.AndThen.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.AndThen.cs
@@ -3,11 +3,15 @@
 public partial struct Option<TSome>
 {
     /// <summary>
-    /// Returns None if the option is None, otherwise calls func with the wrapped value and returns the result.
-    /// Some languages call this operation flatmap.
+    ///     Returns None if the option is None, otherwise calls func with the wrapped value and returns the result.
+    ///     Some languages call this operation flatmap.
     /// </summary>
     /// <param name="func"></param>
     /// <returns></returns>
+    [GitHub(25, "https://github.com/sirh3e/Rust/issues/25")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.and_then")]
+    [Stable("rust1", "1.0.0")]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#1246-1249")]
     public Option<TSome> AndThen(Func<TSome, Option<TSome>> func)
         => Match(func, () => None);
 }
