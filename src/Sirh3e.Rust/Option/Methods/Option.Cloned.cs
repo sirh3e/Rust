638d94155254f1e9dd2ccaef2f4b85a7095f5e49
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-12 13:16:41 +0200

    #26 Option add support for Cloned

--- src/Sirh3e.Rust/Option/Methods/Option.Cloned.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.Cloned.cs
@@ -2,7 +2,14 @@
 
 public partial struct Option<TSome>
 {
-    //ToDo add documentation
+    /// <summary>
+    ///     Maps an Option &TSome to an Option TSome by cloning the contents of the option.
+    /// </summary>
+    /// <returns></returns>
+    [GitHub(26, "https://github.com/sirh3e/Rust/issues/26")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.cloned")]
+    [Stable("rust1", "1.0.0")]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#1775-1777")]
     public Option<TSome> Cloned()
         => Clone();
 }
