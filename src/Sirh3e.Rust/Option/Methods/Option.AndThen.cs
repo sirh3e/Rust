4ccc6ff4e711fa168c4fdcda1a6be7e4e53674de
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-12 14:37:27 +0200

    #25 Added Pure attribute and change it to readonly function

--- src/Sirh3e.Rust/Option/Methods/Option.AndThen.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.AndThen.cs
@@ -8,10 +8,11 @@ public partial struct Option<TSome>
     /// </summary>
     /// <param name="func"></param>
     /// <returns></returns>
+    [Pure]
     [GitHub(25, "https://github.com/sirh3e/Rust/issues/25")]
     [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.and_then")]
     [Stable("rust1", "1.0.0")]
     [Source("https://doc.rust-lang.org/src/core/option.rs.html#1246-1249")]
-    public Option<TSome> AndThen(Func<TSome, Option<TSome>> func)
+    public readonly Option<TSome> AndThen(Func<TSome, Option<TSome>> func)
         => Match(func, () => None);
 }
