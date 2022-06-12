6feb5990961bc92a149e3fff61bd920c37f5b8e6
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-12 15:10:26 +0200

    #31 Added Pure attribute and MethodImpl. Changed function to readonly

--- src/Sirh3e.Rust/Option/Methods/Option.Contains.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.Contains.cs
@@ -3,10 +3,17 @@
 public partial struct Option<TSome>
 {
     /// <summary>
-    /// Returns true if the option is a Some value containing the given value.
+    ///     Returns true if the option is a Some value containing the given value.
     /// </summary>
-    /// <param name="some"></param>
+    /// <param name="other"></param>
+    /// <exception cref="ArgumentNullException">Throws ArgumentNullException when some is null</exception>
     /// <returns></returns>
-    public bool Contains(TSome some)
-        => Match(s => s.Equals(some), () => false);
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [GitHub(31, "https://github.com/sirh3e/Rust/issues/31")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.contains")]
+    [Unstable("option_result_contains", 62358)]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#1621-1623")]
+    public readonly bool Contains(TSome other)
+        => Match(some => (some ?? throw new ArgumentNullException(nameof(some))).Equals(other), () => false);
 }
