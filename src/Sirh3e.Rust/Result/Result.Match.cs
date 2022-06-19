09a0ce4f641d7ef8ed66179ae337a0c81cf1ae07
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-19 23:18:03 +0200

    Refactor Result.Match

--- src/Sirh3e.Rust/Result/Result.Match.cs
+++ src/Sirh3e.Rust/Result/Result.Match.cs
@@ -11,23 +11,9 @@ public readonly partial struct Result<TOk, TErr>
     public void Match(Action<TOk> onOk, Action<TErr> onErr)
     {
         if ( IsOk )
-        {
-            if ( onOk is null )
-            {
-                throw new ArgumentNullException(nameof(onOk));
-            }
-
-            onOk(_ok);
-        }
+            (onOk ?? throw new ArgumentNullException(nameof(onOk)))(_ok);
         else
-        {
-            if ( onErr is null )
-            {
-                throw new ArgumentNullException(nameof(onErr));
-            }
-
-            onErr(_err);
-        }
+            (onErr ?? throw new ArgumentNullException(nameof(onErr)))(_err);
     }
 
     /// <summary>
@@ -39,22 +25,7 @@ public readonly partial struct Result<TOk, TErr>
     /// <returns></returns>
     /// <exception cref="ArgumentNullException"></exception>
     public T Match<T>(Func<TOk, T> onOk, Func<TErr, T> onErr)
-    {
-        if ( IsOk )
-        {
-            if ( onOk is null )
-            {
-                throw new ArgumentNullException(nameof(onOk));
-            }
-
-            return onOk(_ok);
-        }
-
-        if ( onErr is null )
-        {
-            throw new ArgumentNullException(nameof(onErr));
-        }
-
-        return onErr(_err);
-    }
+        => IsOk
+            ? (onOk  ?? throw new ArgumentNullException(nameof(onOk)))(_ok)
+            : (onErr ?? throw new ArgumentNullException(nameof(onErr)))(_err);
 }
