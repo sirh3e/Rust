0df95c3f4840b0733210344aa00f9829580fab07
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-20 23:25:34 +0200

    #34 Added Pure Attribute and MethodImpl to AggressiveInling

--- src/Sirh3e.Rust/Option/Methods/Option.Filter.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.Filter.cs
@@ -3,25 +3,24 @@
 public partial struct Option<TSome>
 {
     /// <summary>
-    /// Returns None if the option is None, otherwise calls predicate with the wrapped value and returns:
-    /// [Some(TSome)] if predicate returns true (where t is the wrapped value), and None if predicate returns false.
-    /// This function works similar to Iterator::filter(). You can imagine the Option&lt;TSome&gt; being an iterator over one or zero elements. filter() lets you decide which elements to keep.
+    ///     Returns None if the option is None, otherwise calls predicate with the wrapped value and returns:
+    ///     [Some(TSome)] if predicate returns true (where t is the wrapped value), and None if predicate returns false.
+    ///     This function works similar to Iterator::filter(). You can imagine the Option&lt;TSome&gt; being an iterator over
+    ///     one or zero elements. filter() lets you decide which elements to keep.
     /// </summary>
     /// <param name="predicate"></param>
     /// <returns></returns>
     /// <exception cref="ArgumentNullException">Throws only if predicate is null</exception>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [GitHub(34, "https://github.com/sirh3e/Rust/issues/34")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.filter")]
+    [Unstable("const_option_ext", 91930)]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#1285-1289")]
     public Option<TSome> Filter(Func<TSome, bool> predicate)
-    {
-        if ( IsNone )
-        {
-            return None;
-        }
-
-        if ( predicate is null )
-        {
-            throw new ArgumentNullException(nameof(predicate));
-        }
-
-        return predicate(_some) ? Some(_some) : None;
-    }
+        => IsSome
+            ? (predicate ?? throw new ArgumentNullException(nameof(predicate)))(_some)
+                ? Some(_some)
+                : None
+            : None;
 }
