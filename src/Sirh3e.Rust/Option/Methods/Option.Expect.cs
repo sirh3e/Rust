0aaee32b96954e4d0afebb6a648a60c0fe2579f1
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-20 23:14:13 +0200

    #32 Added Pure Attribute and MethodImpl to AggressiveInling

--- src/Sirh3e.Rust/Option/Methods/Option.Expect.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.Expect.cs
@@ -3,10 +3,19 @@
 public partial struct Option<TSome>
 {
     /// <summary>
-    /// Returns the contained Some value, consuming the self value.
+    ///     Returns the contained Some value, consuming the self value.
+    ///     Panics
+    ///     Panics if the value is a None with a custom panic message provided by msg.
     /// </summary>
     /// <param name="message"></param>
+    /// <exception cref="PanicException">Panics if the value is a None with a custom panic message provided by msg.</exception>
     /// <returns></returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [GitHub(32, "https://github.com/sirh3e/Rust/issues/32")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.expect")]
+    [Unstable("const_option", 67441)]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#715")]
     public TSome Expect(string message)
-        => Match(s => s, () => ExpectFailed(message));
+        => Match(some => some, () => ExpectFailed(message));
 }
