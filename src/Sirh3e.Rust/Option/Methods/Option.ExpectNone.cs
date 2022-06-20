9968d2ea8067b43e67730dfb10c3cad847f6fe7c
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-20 23:19:01 +0200

    #33 Added Pure Attribute and MethodImpl to AggressiveInling

--- src/Sirh3e.Rust/Option/Methods/Option.ExpectNone.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.ExpectNone.cs
@@ -3,9 +3,12 @@
 public partial struct Option<TSome>
 {
     /// <summary>
-    /// Consumes self while expecting None and returning nothing.
+    ///     Consumes self while expecting None and returning nothing.
     /// </summary>
     /// <param name="message"></param>
+    /// <exception cref="PanicException">Panics if the value is a Some with a custom panic message provided by message.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [GitHub(33, "https://github.com/sirh3e/Rust/issues/33")]
     public void ExpectNone(string message)
         => Match(some => ExpectNoneFailed(message, some), _ => { });
 }
