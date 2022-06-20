85c7877555011d803d809e6d45530dd063ad9c00
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-20 23:35:37 +0200

    #35 Added Pure Attribute and MethodImpl to AggressiveInling

--- src/Sirh3e.Rust/Option/Methods/Option.Flatten.Extension.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.Flatten.Extension.cs
@@ -3,11 +3,19 @@
 public static partial class OptionExtension
 {
     /// <summary>
-    /// Converts from Option&lt;Option&lt;TSome&gt;&gt; to Option&lt;TSome&gt; 
+    ///     Converts from Option<Option<T>> to Option<T>.
     /// </summary>
     /// <param name="option"></param>
     /// <typeparam name="TSome"></typeparam>
     /// <returns></returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [GitHub(35, "https://github.com/sirh3e/Rust/issues/35")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.flatten")]
+    [Stable("option_flattening", "1.40.0")]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#2343")]
     public static Option<TSome> Flatten<TSome>(this Option<Option<TSome>> option)
-        => option.IsNone ? Option<TSome>.None : option.Unwrap();
+        => option.IsNone
+            ? Option<TSome>.None
+            : option.Unwrap();
 }
