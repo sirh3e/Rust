9b9d0d7f0c426fe223d3f82540e42fad753be56a
sirh3e <marvin.bluewin@bluewin.ch>
2022-06-20 23:28:30 +0200

    #24 added readonly method

--- src/Sirh3e.Rust/Option/Methods/Option.And.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.And.cs
@@ -14,7 +14,7 @@ public partial struct Option<TSome>
     [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.and")]
     [Stable("rust1", "1.0.0")]
     [Source("https://doc.rust-lang.org/src/core/option.rs.html#1203-1206")]
-    public Option<T> And<T>(Option<T> option)
+    public readonly Option<T> And<T>(Option<T> option)
         => IsNone
             ? Option<T>.None
             : option;
