d07fe835747e5ac89b79cddbd75ab3438cea134e
sirh3e <marvin.bluewin@bluewin.ch>
2022-09-22 09:31:46 +0200

    #37 Added attributes for Option Insert

--- src/Sirh3e.Rust/Option/Methods/Option.Insert.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.Insert.cs
@@ -9,15 +9,34 @@ public partial struct Option<TSome>
     /// <param name="some"></param>
     /// <returns>Returns the inserted value back</returns>
     /// <exception cref="ArgumentNullException">If the inserted value is null</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [GitHub(37, "https://github.com/sirh3e/Rust/issues/37")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.insert")]
+    [Stable("option_insert", "1.53.0")]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#1437-1439")]
     public TSome Insert(TSome some)
     {
         _ = some ?? throw new ArgumentNullException(nameof(some));
 
-        if ( IsSome && _some is IDisposable disposable )
+        if ( IsSome )
         {
-            disposable.Dispose();
+            {
+                if ( _some is IAsyncDisposable disposable )
+                {
+                    var task = disposable.DisposeAsync();
+                    task.GetAwaiter().GetResult();
+
+                    goto set_some;
+                }
+            }
+            {
+                if ( _some is IDisposable disposable )
+                    disposable.Dispose();
+                goto set_some;
+            }
         }
 
+    set_some:
         SetSome(some);
 
         return _some;
