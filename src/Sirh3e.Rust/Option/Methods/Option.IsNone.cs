964a5d32a0fef5ac11787bdb11c7b688928a94e4
sirh3e <marvin.bluewin@bluewin.ch>
2022-09-22 10:10:18 +0200

    #38 Added attributes for Option IsNone fixed doc url

--- src/Sirh3e.Rust/Option/Methods/Option.IsNone.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.IsNone.cs
@@ -7,7 +7,7 @@ public partial struct Option<TSome>
     /// </summary>
     [Pure]
     [GitHub(38, "https://github.com/sirh3e/Rust/issues/38")]
-    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.insert")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.is_none")]
     [Stable("const_option_basics", "1.48.0")]
     [Source("https://doc.rust-lang.org/src/core/option.rs.html#596")]
     public readonly bool IsNone => IsSome == false;
