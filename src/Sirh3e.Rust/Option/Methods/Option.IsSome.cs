4bb29c03bafd0fab376faabe8667a3515ae46b24
sirh3e <marvin.bluewin@bluewin.ch>
2022-09-22 10:14:19 +0200

    #39 Added attributes for Option IsSome

--- src/Sirh3e.Rust/Option/Methods/Option.IsSome.cs
+++ src/Sirh3e.Rust/Option/Methods/Option.IsSome.cs
@@ -5,5 +5,9 @@ public partial struct Option<TSome>
     /// <summary>
     /// Returns true if the option is a Some value.
     /// </summary>
+    [GitHub(39, "https://github.com/sirh3e/Rust/issues/39")]
+    [Docs("https://doc.rust-lang.org/std/option/enum.Option.html#method.is_some")]
+    [Stable("const_option_basics", "1.48.0")]
+    [Source("https://doc.rust-lang.org/src/core/option.rs.html#553")]
     public bool IsSome { get; private set; }
 }
